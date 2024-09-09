9c8542e669df791ec9a59058cf5e3f10a670f48b
olgafetisova <olga@cow.fi>
2024-09-09 16:20:35 +0100

    Addressing comments

--- cow_amm/cow_amm_lp_and_tvl_4047078.sql
+++ cow_amm/cow_amm_lp_and_tvl_4047078.sql
@@ -18,15 +18,15 @@ with date_range as (
 
 -- Finds the CoW AMM pool address given tokens specified in query parameters (regardless of order) 
 cow_amm_pool as (
-select blockchain, address, token_1_address, token_2_address
-from query_3959044 q
-where ((token_1_address = {{token_a}} and token_2_address = {{token_b}}) or (token_2_address = {{token_a}} and token_1_address = {{token_b}}))
+    select blockchain, address, token_1_address, token_2_address
+    from query_3959044 q
+    where ((token_1_address = {{token_a}} and token_2_address = {{token_b}}) or (token_2_address = {{token_a}} and token_1_address = {{token_b}}))
 )
 
 -- per day lp token total supply changes of the CoW AMM pool by looking at burn/mint events
-, get_lp_balance as (
+, lp_balance_delta as (
     select date(evt_block_time) as day, 
-    sum(case when "from" = 0x0000000000000000000000000000000000000000 THEN (value/1e18) ELSE -(value/1e18) END) as lp_supply
+    sum(case when "from" = 0x0000000000000000000000000000000000000000 THEN value ELSE -value END) as lp_supply
     from erc20_ethereum.evt_transfer
     where contract_address in (select address from cow_amm_pool)
     and ("from" = 0x0000000000000000000000000000000000000000 or "to" = 0x0000000000000000000000000000000000000000)
@@ -34,27 +34,38 @@ where ((token_1_address = {{token_a}} and token_2_address = {{token_b}}) or (tok
 )
 
 , total_lp as (
-select day, lp_supply, sum(lp_supply) over (order by day) as total_lp
-from get_lp_balance
+    select day, lp_supply, sum(lp_supply) over (order by day) as total_lp
+    from lp_balance_delta
 )
 
 -- lp token total supply without date gaps
- , lp_total_supply as (
-SELECT
-        date_range.day,
-        COALESCE(v.total_lp, LAG(v.total_lp) OVER (ORDER BY date_range.day)) AS total_lp
-    FROM
-        date_range
-    LEFT JOIN
-        total_lp v ON date_range.day = v.day
+, lp_total_supply as (
+    select
+        day,
+        total_lp
+    from (
+        -- join full date range with potentially incomplete data. This results in many rows per day (all total supplies on or before that day)
+        -- rank() is then used to order join candidates by recency (rank = 1 is the latest lp supply)
+        select
+            date_range.day,
+            total_lp,
+            rank() over (partition by (date_range.day) order by lp.day desc) as latest
+        from date_range
+        inner join total_lp as lp
+            on date_range.day >= lp.day
+            -- performance optimisation: this assumes one week prior to start there was at least one lp supply change event
+            and lp.day >= (timestamp '{{start}}' - interval '7' day)
+    )
+    where latest = 1
 )
--- Compute tvl by multiplying each day's closing price with bet transfers in and out of the pool
+-- Compute tvl by multiplying each day's closing price with net transfers in and out of the pool
 , get_tvl as (
-SELECT x.day, SUM(amount * price_close) as total_tvl FROM (
-    SELECT date(evt_block_time) as day, 
+    SELECT x.day, SUM(amount * price_close) as total_tvl 
+    FROM (
+        SELECT date(evt_block_time) as day, 
             symbol, 
             -(value/POW(10,decimals)) as amount
-    FROM erc20_ethereum.evt_transfer a LEFT JOIN tokens.erc20 b ON a.contract_address = b.contract_address and blockchain in (select blockchain from cow_amm_pool)
+        FROM erc20_ethereum.evt_transfer a LEFT JOIN tokens.erc20 b ON a.contract_address = b.contract_address and blockchain in (select blockchain from cow_amm_pool)
     where "from"  in (select address from cow_amm_pool)
         and (a.contract_address IN (select token_1_address from cow_amm_pool) or 
         a.contract_address IN (select token_2_address from cow_amm_pool))
@@ -73,19 +84,19 @@ group by 1
 )
 
 , total_tvl as (
-select day, sum(total_tvl) over (order by day) as total_tvl
-from get_tvl
+    select day, sum(total_tvl) over (order by day) as total_tvl
+    from get_tvl
 )
 
 -- With this we can plot the lp token price (tvl/lp total supply) over time
 , final as (
-  select 
-  t1.day, 
-  total_tvl as tvl, 
-  total_lp, 
-  total_tvl/total_lp as lp_token_price
-from total_tvl t1
-inner join lp_total_supply as lp
+    select 
+        t1.day, 
+        total_tvl as tvl, 
+        total_lp, 
+        total_tvl/total_lp as lp_token_price
+    from total_tvl t1
+    inner join lp_total_supply as lp
         on t1.day = lp.day
 )
 
