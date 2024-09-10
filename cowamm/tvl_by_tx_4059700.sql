9968a40989a4016efe6e176500972ff9d4e4dff4
Felix Leupold <felixleupold90@gmail.com>
2024-09-10 20:04:00 +0200

    get rid of cowamm input paramter

---
+++ cowamm/tvl_by_tx_4059700.sql
@@ -0,0 +1,90 @@
+-- Computes the balances and their dollar value of a all CoW AMM pools for a specific token pair at every transaction where it changed
+-- Parameters
+--  {{token_a}} - either token of the pool
+--  {{token_b}} - other token of the pool
+
+with cow_amm_pool as (
+    select
+        created_at,
+        address
+    from query_3959044
+    where ((token_1_address = {{token_a}} and token_2_address = {{token_b}}) or (token_2_address = {{token_a}} and token_1_address = {{token_b}}))
+    order by 1 desc
+),
+
+-- Computes the token balance changes of the relevant token per transaction
+reserves_delta as (
+    select
+        evt_block_time,
+        evt_tx_hash,
+        "from" as pool,
+        contract_address as token,
+        MAX(evt_block_number) as evt_block_number,
+        MAX(evt_index) as evt_index,
+        SUM(-value) as amount
+    from erc20_ethereum.evt_transfer
+    where
+        "from" in (select address from cow_amm_pool)
+        and contract_address in ({{token_a}}, {{token_b}})
+    group by 1, 2, 3, 4
+    union all
+    select
+        evt_block_time,
+        evt_tx_hash,
+        "to" as pool,
+        contract_address as token,
+        MAX(evt_block_number) as evt_block_number,
+        MAX(evt_index) as evt_index,
+        SUM(value) as amount
+    from erc20_ethereum.evt_transfer
+    where
+        "to" in (select address from cow_amm_pool)
+        and contract_address in ({{token_a}}, {{token_b}})
+    group by 1, 2, 3, 4
+),
+
+-- sums token balance changes to get total balances of relevant tokens per transaction
+balances_by_tx as (
+    select
+        evt_block_time,
+        evt_tx_hash,
+        pool,
+        token,
+        SUM(amount) over (partition by (pool, token) order by (evt_block_number, evt_index)) as balance
+    from reserves_delta
+),
+
+-- joins token balances with prices to get tvl of pool per transaction
+tvl as (
+    select
+        evt_block_time as block_time,
+        evt_tx_hash as tx_hash,
+        b.pool,
+        b.token,
+        balance,
+        price,
+        balance * price / POW(10, decimals) as tvl
+    from balances_by_tx as b
+    inner join prices.usd as p
+        on
+            p.minute = DATE_TRUNC('minute', evt_block_time)
+            and b.token = p.contract_address
+)
+
+select
+    tvl1.block_time,
+    tvl1.tx_hash,
+    tvl1.pool,
+    tvl1.token as token1,
+    tvl1.balance as balance1,
+    tvl1.price as price1,
+    tvl2.token as token2,
+    tvl2.balance as balance2,
+    tvl2.price as price2,
+    tvl1.tvl + tvl2.tvl as tvl
+from tvl as tvl1
+inner join tvl as tvl2
+    on
+        tvl1.tx_hash = tvl2.tx_hash
+        and tvl1.pool = tvl2.pool
+        and tvl1.token > tvl2.token
