ca955b3aa491b9f3419eae5a66f412732e40d1d6
olgafetisova <olga@cow.fi>
2024-09-10 13:01:54 +0100

    Adding Balancer CoW AMMs query

---
+++ cow_amm/balance_cow_amms_3959044.sql
@@ -0,0 +1,69 @@
+-- This is part of a base query for monitoring Balancer CoW AMMs
+-- It indexes all Balancer CoW AMMs on ethereum and gnosis
+--
+-- the final table has columns
+-- - blockchain: 'ethereum' or 'gnosis'
+-- - address: address of Balancer CoW AMM
+-- - token_1_address: address of token with smaller address
+-- - token_2_address: address of token with larger address
+
+with cowamm_creations_ethereum as (
+    select varbinary_substring(topic1, 1 + 12, 20) as address
+    from ethereum.logs
+    where (
+        contract_address in (0x23fcC2166F991B8946D195de53745E1b804C91B7, 0xf76c421bAb7df8548604E60deCCcE50477C10462) -- this needs to be changed when a new factory contract is deployed
+        and topic0 = 0x0d03834d0d86c7f57e877af40e26f176dc31bd637535d4ba153d1ac9de88a7ea
+    )
+    and block_time >= cast('2024-07-29 00:00:00' as timestamp)
+),
+
+-- get tokens added via bind (0xe4e1e538) on the CoW AMM address
+cowamms_ethereum as (
+    select
+        'ethereum' as blockchain,
+        contract_address as address,
+        min(varbinary_substring(data, 5 + 2 * 32 + 12, 20)) as token_1_address,
+        max(varbinary_substring(data, 5 + 2 * 32 + 12, 20)) as token_2_address
+    from
+        ethereum.logs
+    where
+        contract_address in (select address from cowamm_creations_ethereum)
+        and topic0 = 0xe4e1e53800000000000000000000000000000000000000000000000000000000
+        and block_time >= cast('2024-07-29 00:00:00' as timestamp)
+    group by 1, 2
+),
+
+-- on gnosis
+cowamm_creations_gnosis as (
+    select varbinary_substring(topic1, 1 + 12, 20) as address
+    from gnosis.logs
+    where (
+        contract_address in (0x23fcC2166F991B8946D195de53745E1b804C91B7, 0x703Bd8115E6F21a37BB5Df97f78614ca72Ad7624) -- this needs to be changed when a new factory contract is deployed
+        and topic0 = 0x0d03834d0d86c7f57e877af40e26f176dc31bd637535d4ba153d1ac9de88a7ea
+    )
+    and block_time >= cast('2024-07-29 00:00:00' as timestamp)
+),
+
+cowamms_gnosis as (
+    select
+        'gnosis' as blockchain,
+        contract_address as address,
+        min(varbinary_substring(data, 5 + 2 * 32 + 12, 20)) as token_1_address,
+        max(varbinary_substring(data, 5 + 2 * 32 + 12, 20)) as token_2_address
+    from
+        gnosis.logs
+    where
+        contract_address in (select address from cowamm_creations_gnosis)
+        and topic0 = 0xe4e1e53800000000000000000000000000000000000000000000000000000000
+        and block_time >= cast('2024-07-29 00:00:00' as timestamp)
+    group by 1, 2
+),
+
+-- combine data for different chains
+cowamms as (
+    select * from cowamms_ethereum
+    union all
+    select * from cowamms_gnosis
+)
+
+select * from cowamms
