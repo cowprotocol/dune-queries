5c0690a885716d3d46ca0e61df644c8dc9bf3310
Felix Leupold <felixleupold90@gmail.com>
2024-09-10 20:33:44 +0200

    use tvl query from #27

---
+++ cowamm/profitability/invariant_growth/cow_amm_4059213.sql
@@ -0,0 +1,30 @@
+-- Computes the absolute surplus per $100 tvl per day (aka its invariant growth) for a CoW AMM.
+-- Parameters
+--  {{token_a}} - either token of the desired uni pool
+--  {{token_b}} - other token of the desired uni pool
+--  {{start}} - date as of which the analysis should run
+
+-- Finds the CoW AMM pool address given tokens specified in query parameters (regardless of order) 
+with cow_amm_pool as (
+    select
+        created_at,
+        address
+    from query_3959044
+    where ((token_1_address = {{token_a}} and token_2_address = {{token_b}}) or (token_2_address = {{token_a}} and token_1_address = {{token_b}}))
+    order by 1 desc
+    limit 1
+)
+
+-- computes, surplus, tvl and thus relative surplus (per $100)
+select
+    block_date,
+    SUM(surplus_usd) as absolute_invariant_growth,
+    AVG(tvl) as tvl,
+    SUM(surplus_usd / tvl) * 100 as pct_invariant_growth
+from cow_protocol_ethereum.trades as t
+inner join "query_4059700(token_a='{{token_a}}', token_b='{{token_b}}')" as tvl
+    on
+        t.tx_hash = tvl.tx_hash
+        and tvl.pool = trader
+        and trader = (select address from cow_amm_pool)
+group by 1
