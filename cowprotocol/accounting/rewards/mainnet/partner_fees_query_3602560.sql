aeea09d5392e3d4f16e6e95b4657e5c387ec5186
harisang <harisangelidakis@gmail.com>
2024-09-12 12:17:56 +0300

    add description and invoke block range query

--- cowprotocol/accounting/rewards/mainnet/partner_fees_query_3602560.sql
+++ cowprotocol/accounting/rewards/mainnet/partner_fees_query_3602560.sql
@@ -1,7 +1,15 @@
+-- This query computes the partner fees associated with each partner in a given time interval
+--
+-- Parameters:
+--  {{start_time}} - the timestamp for which the accounting should start (inclusively)
+--  {{end_time}} - the timestamp for which the accounting should end (exclusively)
+--  {{result}} - two views of the result, one aggregated and one on a per tx basis
+
 with
 per_settlement_prelim as (
     select
         t.block_time,
+        t.block_number,
         t.order_uid,
         t.tx_hash,
         r.data.partner_fee_recipient as partner_recipient,  -- noqa: RF01
@@ -35,8 +43,8 @@ per_settlement_prelim as (
             and t.tx_hash = from_hex(r.tx_hash)
     where
         json_extract(a.encode, '$.metadata.partnerFee.recipient') is not null
-        and t.block_time >= cast('{{start_time}}' as timestamp)
-        and t.block_time < cast('{{end_time}}' as timestamp)
+        and t.block_number >= (select start_block from "query_3333356(start_time='2018-01-01 00:00',end_time='{{end_time}}')")
+        and t.block_number < (select end_block from "query_3333356(start_time='2018-01-01 00:00',end_time='{{end_time}}')")
     order by
         t.block_time desc
 ),
@@ -53,11 +61,11 @@ aggregate_per_recipient as (
     select
         partner_recipient,
         case
-            when partner_recipient = '0x63695Eee2c3141BDE314C5a6f89B98E62808d716' then sum(0.9 * raw_integrator_fee_in_eth)
+            when partner_recipient = '0x63695Eee2c3141BDE314C5a6f89B98E62808d716' then sum(0.9 * raw_integrator_fee_in_eth)  -- this is for the Safe integration
             else sum(0.85 * raw_integrator_fee_in_eth)
         end as partner_fee_part,
         case
-            when partner_recipient = '0x63695Eee2c3141BDE314C5a6f89B98E62808d716' then sum(0.1 * raw_integrator_fee_in_eth)
+            when partner_recipient = '0x63695Eee2c3141BDE314C5a6f89B98E62808d716' then sum(0.1 * raw_integrator_fee_in_eth)  -- this is for the Safe integratio
             else sum(0.15 * raw_integrator_fee_in_eth)
         end as cow_dao_partner_fee_part
     from
