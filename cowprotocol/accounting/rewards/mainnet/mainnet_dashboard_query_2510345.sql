7cc08aea4e0aa1fc0a9e68ed4ceffccc7dbb4d8d
harisang <harisangelidakis@gmail.com>
2024-09-16 17:46:52 +0300

    add mainnnet payouts dashboard

---
+++ cowprotocol/accounting/rewards/mainnet/mainnet_dashboard_query_2510345.sql
@@ -0,0 +1,253 @@
+with
+
+block_range as (
+    select * from "query_3333356(start_time='{{start_time}}',end_time='{{end_time}}')"
+),
+
+results_per_tx as (
+    select * from "query_3427730(start_time='{{start_time}}',end_time='{{end_time}}',cte_name='results_per_tx')"
+
+),
+
+solver_slippage as (
+    select
+        solver_address as solver,
+        sum(eth_slippage_wei) * 1.0 / pow(10, 18) as slippage
+    from results_per_tx
+    group by solver_address
+),
+-- end SLIPPAGE
+
+-- BEGIN SOLVER REWARDS: https://dune.com/queries/2283297
+batch_rewards_temp as (
+    select 
+        block_deadline,
+        block_number,  -- Null here means the settlement did not occur.
+        from_hex(solver) as winning_solver,
+        from_hex(tx_hash) as tx_hash,
+        -- Unpacking the data
+        cast(cast(data.winning_score as varchar) as int256) as winning_score,
+        cast(cast(data.reference_score as varchar) as int256) as reference_score,
+        cast(cast(data.surplus as varchar) as int256) as surplus,
+        cast(cast(data.fee as varchar) as int256) as fee,
+        cast(cast(data.execution_cost as varchar) as int256) as execution_cost,
+        cast(cast(data.uncapped_payment_eth as varchar) as int256) as uncapped_payment_eth,
+        case
+            when (block_deadline >= 20413283 and block_deadline <= 20413965 and cast(cast(data.capped_payment as varchar) as int256) < 0) then 0
+            else cast(cast(data.capped_payment as varchar) as int256)
+        end as capped_payment,
+        transform(data.participating_solvers, x -> from_hex(x)) as participating_solvers,
+        cardinality(data.participating_solvers) as num_participants
+    from cowswap.raw_batch_rewards
+    where block_deadline > (select start_block from block_range)
+    and block_deadline <= (select end_block from block_range)
+),
+
+batch_rewards as (
+    select
+        block_deadline,
+        block_number,
+        winning_solver,
+        tx_hash,
+        winning_score,
+        reference_score,
+        surplus,
+        fee,
+        execution_cost,
+        capped_payment,
+        participating_solvers,
+        num_participants
+    from batch_rewards_temp
+),
+
+participation_data as (
+    select
+        tx_hash,
+        participant,
+        case
+            when block_deadline <= 20365510 then 1  -- final block deadline of accounting week of July 16 - July 23, 2024
+            else 0
+        end as participation_count
+    from batch_rewards
+    cross join unnest(participating_solvers) as t(participant)
+),
+
+participation_counts as (
+    select
+        participant as solver,
+        sum(participation_count) as num_participating_batches
+    from participation_data
+    group by participant
+),
+
+-- AKA Performance Rewards
+primary_rewards as (
+    select
+        winning_solver as solver,
+        cast(sum(capped_payment) as double) as reward_wei
+        -- ,cast(SUM(execution_cost) as double) as exececution_cost_wei
+    from batch_rewards
+    group by winning_solver
+),
+
+fees_and_costs as (
+    select
+        winning_solver as solver,
+        cast(sum(fee) as double) as network_fee_wei,
+        cast(sum(execution_cost) as double) as exececution_cost_wei
+    from batch_rewards
+    group by winning_solver
+),
+
+conversion_prices as (
+    select
+        (
+            select avg(
+                case
+                    when price > 10 then 0.26 -- dirty fix for some bogus COW prices Dune reports on July 29, 2024
+                    else price
+                end
+            ) from prices.usd
+            where
+                blockchain = 'ethereum'
+                and contract_address = 0xdef1ca1fb7fbcdc777520aa7f396b4e015f497ab
+                and date(minute) = cast('{{end_time}}' as timestamp) - interval '1' day
+        ) as cow_price,
+        (
+            select avg(price) from prices.usd
+            where
+                blockchain = 'ethereum'
+                and contract_address = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+                and date(minute) = cast('{{end_time}}' as timestamp) - interval '1' day
+        ) as eth_price
+),
+
+-- BEGIN QUOTE REWARDS
+order_quotes as (
+    select
+        order_uid,
+        quote_solver
+    from cow_protocol_ethereum.order_rewards
+    where
+        block_number > (select start_block from block_range)
+        and block_number <= (select end_block from block_range)
+),
+
+winning_quotes as (
+    select
+        oq.order_uid,
+        quote_solver as solver
+    from order_quotes oq
+    inner join cow_protocol_ethereum.trades t on oq.order_uid = t.order_uid and oq.quote_solver != 0x0000000000000000000000000000000000000000
+),
+
+quote_rewards as (
+    select
+        solver,
+        least({{quote_reward}}, {{quote_cap_eth}} * (select eth_price / cow_price from conversion_prices)) * count(*) as quote_reward
+    from winning_quotes group by solver
+),
+
+pre_results as (
+    select 
+        pc.solver,
+        coalesce(reward_wei, 0) / pow(10, 18) as reward_eth,
+        coalesce(network_fee_wei, 0) / pow(10, 18) as network_fee_eth,
+        coalesce(exececution_cost_wei, 0) / pow(10, 18) as execution_cost_eth,
+        coalesce(reward_wei, 0) / pow(10, 18) * (select eth_price / cow_price from conversion_prices) as reward_cow,
+        num_participating_batches
+    from participation_counts pc
+    left outer join primary_rewards pr
+    on pr.solver = pc.solver
+    left outer join fees_and_costs fc
+    on fc.solver = pc.solver
+),
+
+
+aggregate_results as (
+    select 
+        solver,
+        -- payment_eth,
+        reward_eth as primary_reward_eth,
+        reward_cow as primary_reward_cow,
+        network_fee_eth,
+        execution_cost_eth,
+        0 as secondary_reward_cow,
+        0 as secondary_reward_eth,
+        num_participating_batches
+    from pre_results
+),
+
+combined_data as (
+    select 
+        coalesce(ar.solver, ss.solver, qr.solver) as solver,
+        -- coalesce(ar.solver, qr.solver) as solver,
+        -- payment_eth,
+        network_fee_eth,
+        execution_cost_eth,
+        primary_reward_eth,
+        primary_reward_cow,
+        secondary_reward_cow,
+        secondary_reward_eth,
+        num_participating_batches,
+        coalesce(quote_reward, 0) as quote_reward,
+        coalesce(slippage, 0) as slippage_eth,
+        -- 0 as slippage_eth,
+        concat(
+            '<a href="https://dune.com/queries/2332678?SolverAddress=',
+            cast(ar.solver as varchar),
+            '&CTE_NAME=results_per_tx&StartTime={{start_time}}&endTime={{end_time}}&MinAbsoluteSlippageTolerance=0&RelativeSlippageTolerance=0&SignificantSlippageValue=0" target="_blank">link</a>'
+        ) as slippage_per_tx,
+        concat(environment, '-', name) as name
+    from aggregate_results ar
+    full outer join solver_slippage ss
+        on ar.solver = ss.solver
+    full outer join quote_rewards qr
+        on ar.solver = qr.solver
+    left join cow_protocol_ethereum.solvers s
+        on coalesce(ar.solver, ss.solver, qr.solver) = address
+        -- on coalesce(ar.solver, qr.solver) = address
+),
+
+extended_payout_data as (
+    select 
+        cd.*,
+        -- computed fields used to simplify case logic.
+        primary_reward_eth + secondary_reward_eth + slippage_eth + network_fee_eth as total_outgoing_eth,
+        case when primary_reward_eth + secondary_reward_eth + slippage_eth + network_fee_eth < 0 then true else false end as is_overdraft,
+        slippage_eth + network_fee_eth as reimbursement_eth,
+        (slippage_eth + network_fee_eth) * (select eth_price / cow_price from conversion_prices) as reimbursement_cow,
+        primary_reward_cow + secondary_reward_cow as total_cow_reward,
+        primary_reward_eth + secondary_reward_eth as total_eth_reward
+    from combined_data cd
+),
+
+-- Implement the logic contained in 
+-- https://github.com/cowprotocol/solver-rewards/blob/9838116e5253263e78e5b5777106458b541beb71/src/fetch/payouts.py#L136-L217
+final_results as (
+    select 
+        epd.*,
+        case 
+            when is_overdraft then null
+            when reimbursement_eth > 0 and total_cow_reward < 0
+                then reimbursement_eth + total_eth_reward
+            when reimbursement_eth < 0 and total_cow_reward > 0
+                then 0
+            else reimbursement_eth
+        end as eth_transfer,
+        case 
+            when is_overdraft then null
+            when reimbursement_eth > 0 and total_cow_reward < 0
+                then 0
+            when reimbursement_eth < 0 and total_cow_reward > 0
+                then reimbursement_cow + total_cow_reward
+            else total_cow_reward
+        end as cow_transfer,
+        case when is_overdraft then total_outgoing_eth else null end as overdraft,
+        reward_target
+    from extended_payout_data epd
+        left join named_results nr
+        on epd.solver = nr.solver
+)
+
+select * from final_results
