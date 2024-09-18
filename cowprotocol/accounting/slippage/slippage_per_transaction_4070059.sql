cf754bf8a415b864b71a4f7a0035d568e8a64bdc
Felix Henneke <felix.henneke@protonmail.com>
2024-09-18 13:37:44 +0200

    exclude settlements with missing prices

--- cowprotocol/accounting/slippage/slippage_per_transaction_4070059.sql
+++ cowprotocol/accounting/slippage/slippage_per_transaction_4070059.sql
@@ -10,9 +10,10 @@
 -- - tx_hash: settlement transaction hash
 -- - solver_address: address of the solver executing the settlement
 -- - slippage_usd: USD value of slippage
--- - slippage_native_atom: value of slippage in atoms of native token
+-- - slippage_native: value of slippage in native token
 --
 -- Results of the query are filtered to not include batches from excluded_batches.
+-- Batches are also excluded if there is a non-zero imbalance and no value (in native atomes).
 
 with excluded_batches as (
     select tx_hash from query_3490353
@@ -29,3 +30,4 @@ inner join cow_protocol_{{blockchain}}.batches as b
     on s.tx_hash = b.tx_hash
 where s.tx_hash not in (select tx_hash from excluded_batches)
 group by 1, 2, 3
+having bool_and(slippage_native_atom is not null or slippage_atoms = 0)
