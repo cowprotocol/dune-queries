075ecbb3a9baf1a1f40c6178b7d55292e07ecf42
Felix Henneke <felix.henneke@protonmail.com>
2024-09-23 16:41:13 +0200

    [EASY] Change slippage wrapper for consistency with old slippage query (#49)
    * fix parameter to ethereum for current slippage accounting
    
    * make output column of eth_slippage_wei a float

--- cowprotocol/accounting/slippage/slippage_query_3427730.sql
+++ cowprotocol/accounting/slippage/slippage_query_3427730.sql
@@ -2,15 +2,15 @@ select
     solver_address,
     concat(environment, '-', name) as solver_name,
     slippage_usd as usd_value,
-    slippage_wei as eth_slippage_wei,
+    1.0 * slippage_wei as eth_slippage_wei,
     concat(
         '<a href="https://dune.com/queries/4070065',
-        '&blockchain={{blockchain}}',
+        '&blockchain=ethereum',
         '&start_time={{start_time}}',
         '&end_time={{end_time}}',
         '&slippage_table_name=slippage_per_transaction',
         '" target="_blank">link</a>'
     ) as slippage_per_transaction
-from "query_4070065(blockchain='{{blockchain}}',start_time='{{start_time}}',end_time='{{end_time}}',slippage_table_name='slippage_per_solver')"
-inner join cow_protocol_{{blockchain}}.solvers
+from "query_4070065(blockchain='ethereum',start_time='{{start_time}}',end_time='{{end_time}}',slippage_table_name='slippage_per_solver')"
+inner join cow_protocol_ethereum.solvers
     on solver_address = address
