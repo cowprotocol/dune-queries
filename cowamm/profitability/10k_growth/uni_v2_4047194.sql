7495c1ceaf49f7a33bcb9c40dfe354b88768a131
Felix Leupold <felixleupold90@gmail.com>
2024-10-01 14:44:41 +0200

    extract common subquery

--- cowamm/profitability/10k_growth/uni_v2_4047194.sql
+++ cowamm/profitability/10k_growth/uni_v2_4047194.sql
@@ -16,43 +16,11 @@ with date_range as (
         )) t (day) --noqa: AL01
 ),
 
--- Finds the uniswap v2 pool address given tokens specified in query parameters (regardless of order)
-pools as (
-    select
-        substr(data, 13, 20) as contract_address,
-        substr(topic1, 13, 20) as token0,
-        substr(topic2, 13, 20) as token1
-    from {{blockchain}}.logs
-    where
-        topic0 = 0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9 -- PairCreated
-        -- topic1: 0x0...0<token0>, topic2: 0x0...0<token1>
-        and ((substr(topic1, 13, 20) = {{token_a}} and substr(topic2, 13, 20) = {{token_b}}) or (substr(topic2, 13, 20) = {{token_a}} and substr(topic1, 13, 20) = {{token_b}}))
-),
-
-syncs as (
-    select
-        tx_hash as evt_tx_hash,
-        index as evt_index,
-        block_time as evt_block_time,
-        block_number as evt_block_number,
-        contract_address,
-        varbinary_to_uint256(substr(data, 1, 32)) as reserve0,
-        varbinary_to_uint256(substr(data, 33, 32)) as reserve1,
-        rank() over (partition by (contract_address) order by block_time desc) as latest
-    from {{blockchain}}.logs
-    where
-        topic0 = 0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1 -- Sync
-        and contract_address in (select contract_address from pools)
-),
-
 -- select the pool with the largest latest k
 pool as (
-    select pools.*
-    from pools
-    inner join syncs
-        on
-            pools.contract_address = syncs.contract_address
-            and latest = 1
+    select contract_address, token0, token1
+    from "query_4117043(blockchain='{{blockchain}}',token_a='{{token_a}}',token_b='{{token_b}}')"
+    where latest = 1
     order by (reserve0 * reserve1) desc
     limit 1
 ),
@@ -112,12 +80,12 @@ latest_syncs_per_day as (
     select
         day,
         pool.contract_address,
-        token0,
+        pool.token0,
         reserve0,
-        token1,
+        pool.token1,
         reserve1,
         rank() over (partition by (date_range.day) order by (evt_block_number, evt_index) desc) as latest
-    from syncs
+    from "query_4117043(blockchain='{{blockchain}}',token_a='{{token_a}}',token_b='{{token_b}}')" syncs
     inner join date_range
         on day >= date(evt_block_time)
     inner join pool
--- cowamm/profitability/invariant_growth/uni_v2_4060136.sql
+++ cowamm/profitability/invariant_growth/uni_v2_4060136.sql
@@ -14,43 +14,11 @@ with date_range as (
         )) t (day) --noqa: AL01
 ),
 
--- Finds the uniswap v2 pool address given tokens specified in query parameters (regardless of order)
-pools as (
-    select
-        substr(data, 13, 20) as contract_address,
-        substr(topic1, 13, 20) as token0,
-        substr(topic2, 13, 20) as token1
-    from {{blockchain}}.logs
-    where
-        topic0 = 0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9 -- PairCreated
-        -- topic1: 0x0...0<token0>, topic2: 0x0...0<token1>
-        and ((substr(topic1, 13, 20) = {{token_a}} and substr(topic2, 13, 20) = {{token_b}}) or (substr(topic2, 13, 20) = {{token_a}} and substr(topic1, 13, 20) = {{token_b}}))
-),
-
-syncs as (
-    select
-        tx_hash as evt_tx_hash,
-        index as evt_index,
-        block_time as evt_block_time,
-        block_number as evt_block_number,
-        contract_address,
-        varbinary_to_uint256(substr(data, 1, 32)) as reserve0,
-        varbinary_to_uint256(substr(data, 33, 32)) as reserve1,
-        rank() over (partition by (contract_address) order by block_time desc) as latest
-    from {{blockchain}}.logs
-    where
-        topic0 = 0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1 -- Sync
-        and contract_address in (select contract_address from pools)
-),
-
 -- select the pool with the largest latest k
 pool as (
-    select pools.*
-    from pools
-    inner join syncs
-        on
-            pools.contract_address = syncs.contract_address
-            and latest = 1
+    select contract_address, token0, token1
+    from "query_4117043(blockchain='{{blockchain}}',token_a='{{token_a}}',token_b='{{token_b}}')"
+    where latest = 1
     order by (reserve0 * reserve1) desc
     limit 1
 ),
@@ -80,7 +48,7 @@ tvl_volume_per_swap as (
         (amount0In * p0.price / pow(10, p0.decimals)) + (amount1In * p1.price / pow(10, p1.decimals)) as volume_in,
         (amount0Out * p0.price / pow(10, p0.decimals)) + (amount1Out * p1.price / pow(10, p1.decimals)) as volume_out,
         (reserve0 * p0.price / pow(10, p0.decimals)) + (reserve1 * p1.price / pow(10, p1.decimals)) as tvl
-    from syncs
+    from "query_4117043(blockchain='{{blockchain}}',token_a='{{token_a}}',token_b='{{token_b}}')" syncs
     inner join swaps
         on
             syncs.evt_tx_hash = swaps.evt_tx_hash
@@ -91,11 +59,11 @@ tvl_volume_per_swap as (
     inner join prices.usd as p0
         on
             date_trunc('minute', syncs.evt_block_time) = p0.minute
-            and p0.contract_address = token0
+            and p0.contract_address = syncs.token0
     inner join prices.usd as p1
         on
             date_trunc('minute', syncs.evt_block_time) = p1.minute
-            and p1.contract_address = token1
+            and p1.contract_address = syncs.token1
     where syncs.evt_block_time >= date(timestamp '{{start}}')
 )
 
