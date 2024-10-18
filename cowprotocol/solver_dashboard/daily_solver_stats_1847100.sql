72a3524cc3db3b55d6abae83dd42fa05b45ed90b
pauljanicot@yahoo.com <pauljanicot@yahoo.com>
2024-10-18 09:30:42 +0100

    Regrouped the query in a single query

--- cowprotocol/solver_dashboard/daily_solver_stats_1847100.sql
+++ cowprotocol/solver_dashboard/daily_solver_stats_1847100.sql
@@ -1,34 +1,21 @@
-WITH solver_info as (
-    SELECT 
-        date_trunc('{{Aggregate by}}', block_date) as day,
-        name,
-        count(*) as batches_solved,
-        sum(dex_swaps) as dex_swaps,
-        sum(num_trades) as num_trades,
-        sum(gas_used) as gas_used,
-        sum(batch_value) as total_batch_value,
-        avg(batch_value) as average_batch_volume,
-        avg(num_trades) as average_batch_size
-    FROM cow_protocol_ethereum.batches
-    JOIN cow_protocol_ethereum.solvers 
-        ON solver_address = address
-    WHERE environment = 'prod'
-        AND block_date > cast('2022-03-01' as timestamp)
-        -- AND active = True
-    GROUP BY 
-        name,
-        date_trunc('{{Aggregate by}}', block_date)
-)
-SELECT 
-    day,
+SELECT date_trunc('{{Aggregate by}}', block_date) as day,
     name as solver_name,
-    batches_solved,
-    num_trades,
-    dex_swaps,
-    1.0 * gas_used / num_trades as average_gas_per_trade,
-    1.0 * dex_swaps / num_trades as average_dex_swaps_per_trade,
-    average_batch_size,
-    total_batch_value,
-    average_batch_volume
-FROM solver_info
-ORDER BY num_trades DESC
+    -- concat(environment, '-', name) as name,
+    max(block_time) last_solution,
+    count(*) as batches_solved,
+    sum(dex_swaps) as dex_swaps,
+    sum(num_trades) as num_trades,
+    sum(gas_used) as gas_used,
+    sum(batch_value) as total_batch_value,
+    avg(batch_value) as average_batch_volume,
+    avg(num_trades) as average_batch_size,
+    1.0 * sum(gas_used) / sum(num_trades) as average_gas_per_trade,
+    1.0 * sum(dex_swaps) / sum(num_trades) as average_dex_swaps_per_trade,
+    FROM cow_protocol_ethereum.batches
+    JOIN cow_protocol_ethereum.solvers ON solver_address = address
+WHERE environment in ('prod', 'barn')
+    AND block_date > now() - interval '{{LastNDays}}' day
+    AND active = True
+GROUP BY name,
+    date_trunc('{{Aggregate by}}', block_date)
+ORDER BY num_trades desc
