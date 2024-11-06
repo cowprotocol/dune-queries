ce90b38d4880c01647056273d2ad195746888e03
pauljanicot@yahoo.com <pauljanicot@yahoo.com>
2024-11-06 15:48:35 +0000

    A lot of formatting and removed some commented lines

--- cowprotocol/solver_dashboard/batch_settlement_info_1373033.sql
+++ cowprotocol/solver_dashboard/batch_settlement_info_1373033.sql
@@ -1,21 +1,20 @@
--- The fee value and cost coverage is no longer used in the current mechanism
 -- Parameters
 -- {{blockchain}} - blockchain name
 
-WITH 
+with 
 num_tokens_traded as (
     select 
-        hash,
+        tx_hash,
         count(distinct(token)) num_tokens 
     from (
-        select evt_tx_hash as hash, "buyToken" token from gnosis_protocol_v2_{{blockchain}}.GPv2Settlement_evt_Trade
-        UNION
-        select evt_tx_hash as hash, "sellToken" token from gnosis_protocol_v2_{{blockchain}}.GPv2Settlement_evt_Trade
+        select evt_tx_hash as tx_hash, "buyToken" token from gnosis_protocol_v2_{{blockchain}}.GPv2Settlement_evt_Trade
+        union
+        select evt_tx_hash as tx_hash, "sellToken" token from gnosis_protocol_v2_{{blockchain}}.GPv2Settlement_evt_Trade
     ) as all_tokens
-    group by hash
+    group by tx_hash
 )
 
-SELECT 
+select 
     block_time,
     num_trades,
     num_tokens,
@@ -28,26 +27,25 @@ SELECT
         CONCAT('<a href="https://gnosisscan.io/address/', cast(solver_address as varchar), '" target="_blank">', concat(environment, '-', name),  '</a>') 
         when '{{blockchain}}'='arbitrum' then
         CONCAT('<a href="https://arbiscan.io/address/', cast(solver_address as varchar), '" target="_blank">', concat(environment, '-', name),  '</a>') 
-        end as solver,
+    end as solver,
     
     case when '{{blockchain}}'='ethereum' then
-        CONCAT('<a href="https://etherscan.io/tx/', cast(tx_hash as varchar), '" target="_blank">', cast(tx_hash as varchar),  '</a>') 
+        CONCAT('<a href="https://etherscan.io/tx/', cast(b.tx_hash as varchar), '" target="_blank">', cast(b.tx_hash as varchar),  '</a>') 
         when '{{blockchain}}'='gnosis' then
-        CONCAT('<a href="https://gnosisscan.io/tx/', cast(tx_hash as varchar), '" target="_blank">', cast(tx_hash as varchar),  '</a>') 
+        CONCAT('<a href="https://gnosisscan.io/tx/', cast(b.tx_hash as varchar), '" target="_blank">', cast(b.tx_hash as varchar),  '</a>') 
         when '{{blockchain}}'='arbitrum' then
-        CONCAT('<a href="https://arbiscan.io/tx/', cast(tx_hash as varchar), '" target="_blank">', cast(tx_hash as varchar),  '</a>') 
-    end as txHash,
+        CONCAT('<a href="https://arbiscan.io/tx/', cast(b.tx_hash as varchar), '" target="_blank">', cast(b.tx_hash as varchar),  '</a>') 
+    end as tx_hash,
     gas_price / pow(10, 9) as gas_price,
     gas_used,
     tx_cost_usd,
     call_data_size,
     unwraps,
     token_approvals
-    --case when tx_cost_usd > 0 then fee_value / tx_cost_usd else null end as coverage 
-FROM cow_protocol_{{blockchain}}.batches b
-JOIN num_tokens_traded
-    ON hash = tx_hash
-JOIN cow_protocol_{{blockchain}}.solvers
+from cow_protocol_{{blockchain}}.batches b
+join num_tokens_traded n
+    on n.tx_hash = b.tx_hash
+join cow_protocol_{{blockchain}}.solvers
     on solver_address = address
 where block_time > now() - interval '3' month
-ORDER BY block_time DESC;
+order by block_time desc;
