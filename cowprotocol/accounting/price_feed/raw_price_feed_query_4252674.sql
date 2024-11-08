0ae2cfb4c22efbb9acea20c5f082c282f98c4b5d
harisang <harisangelidakis@gmail.com>
2024-11-08 03:14:34 +0200

    addressed comments

--- cowprotocol/accounting/price_feed/raw_price_feed_query_4252674.sql
+++ cowprotocol/accounting/price_feed/raw_price_feed_query_4252674.sql
@@ -8,7 +8,7 @@ with imported_prices as (
     from dune.cowprotocol.dataset_price_feed_ethereum
 ),
 
-processed_data as (
+imported_prices_per_minute as (
     select -- noqa: ST06
         token_address,
         date_trunc('minute', time) as minute, --noqa: RF04
@@ -18,16 +18,16 @@ processed_data as (
     from imported_prices group by 1, 2, 3, 4
 ),
 
-processed_data_with_usd_prices as (
+imported_prices_per_minute_with_usd_prices as (
     select -- noqa: ST06
-        pd.token_address,
-        pd.minute,
-        pd.decimals,
-        pd.price_unit_eth,
-        pd.price_unit_eth * p.price as price_unit,
-        pd.price_unit_eth * p.price / pow(10, pd.decimals) as price_atom,
+        ippm.token_address,
+        ippm.minute,
+        ippm.decimals,
+        ippm.price_unit_eth,
+        ippm.price_unit_eth * p.price as price_unit,
+        ippm.price_unit_eth * p.price / pow(10, ippm.decimals) as price_atom,
         source
-    from processed_data as pd inner join prices.usd as p on pd.minute = p.minute
+    from imported_prices_per_minute as ippm inner join prices.usd as p on ippm.minute = p.minute
     where p.contract_address = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2 and blockchain = 'ethereum'
 )
 
@@ -38,4 +38,4 @@ select -- noqa: ST06
     source,
     avg(price_unit) as price_unit,
     avg(price_atom) as price_atom
-from processed_data_with_usd_prices group by 1, 2, 3, 4
+from imported_prices_per_minute_with_usd_prices group by 1, 2, 3, 4
