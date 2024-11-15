9fc431adf456f0a921273120388acfb2aa738a06
pauljanicot@yahoo.com <pauljanicot@yahoo.com>
2024-11-15 11:56:46 +0000

    Simplified the querying of all tranactions from a curve pool

--- cowamm/profitability/competitor_kpis/curve/curve_largest_2token_pools_4232976.sql
+++ cowamm/profitability/competitor_kpis/curve/curve_largest_2token_pools_4232976.sql
@@ -1,65 +1,82 @@
 -- Finds all the curve pools with 2 tokens and their TVLs
 -- Input: blockchain
 
-with 
+with
 -- filters pools with 2 tokens
-pools as(
-select pool_address as contract_address,
-coin0 as token0,
-coin1 as token1,
-mid_fee*power(10,-10) as fee
-from curvefi_{{blockchain}}.view_pools
-where coin2 = 0x0000000000000000000000000000000000000000),
+pools as (
+    select
+        pool_address as contract_address,
+        coin0 as token0,
+        coin1 as token1,
+        mid_fee * power(10, -10) as fee
+    from curvefi_{{blockchain}}.view_pools
+    where coin2 = 0x0000000000000000000000000000000000000000
+),
 
 -- finds all transfers in and out of the pools to rebuild the reserves
-transfers as(
-select p.contract_address, token0, token1, -value as transfer0, 0 as transfer1, evt_index, evt_tx_hash as tx_hash, evt_block_time as time, fee
-    from pools p
-    join erc20_{{blockchain}}.evt_transfer t0
-    on p.contract_address = t0."from"
-    and p.token0 = t0.contract_address
-union
-select p.contract_address, token0, token1, value as transfer0, 0 as transfer1, evt_index, evt_tx_hash as tx_hash, evt_block_time as time, fee
-    from pools p
-    join erc20_{{blockchain}}.evt_transfer t0
-    on p.contract_address = t0.to
-    and p.token0 = t0.contract_address
-union
-select p.contract_address, token0, token1, 0 as transfer0, -value as transfer1, evt_index, evt_tx_hash as tx_hash, evt_block_time as time, fee
-    from pools p
-    join erc20_{{blockchain}}.evt_transfer t1
-    on p.contract_address = t1."from"
-    and p.token1 = t1.contract_address
-union
-select p.contract_address, token0, token1, 0 as transfer0, value as transfer1, evt_index, evt_tx_hash as tx_hash, evt_block_time as time, fee
-    from pools p
-    join erc20_{{blockchain}}.evt_transfer t1
-    on p.contract_address = t1.to
-    and p.token1 = t1.contract_address),
+transfers as (
+    select
+        p.contract_address,
+        token0,
+        token1,
+        evt_block_time as block_time,
+        evt_index,
+        evt_tx_hash as tx_hash,
+        fee,
+        case
+            when t.contract_address = token0 and "from" = p.contract_address then -value
+            when t.contract_address = token0 and to = p.contract_address then value else 0
+        end as transfer0,
+        case
+            when t.contract_address = token1 and "from" = p.contract_address then -value
+            when t.contract_address = token1 and to = p.contract_address then value else 0
+        end as transfer1
+    from erc20_{{blockchain}}.evt_transfer as t
+    inner join pools as p
+        on
+            (
+                t."from" = p.contract_address
+                or t.to = p.contract_address
+            )
+            and (token0 = t.contract_address or token1 = t.contract_address)
+),
 
 -- rebuilds the reserves from the transfers
 -- ETH transfers are not considered  
-reserves as(
-select contract_address, token0, token1, tx_hash, time,
-sum(transfer0) over (partition by contract_address order by time, evt_index) as reserve0,
-sum(transfer1) over (partition by contract_address order by time, evt_index) as reserve1,
-fee,
-ROW_NUMBER() OVER (PARTITION BY tx_hash,contract_address ORDER BY evt_index DESC) AS row_num,
-ROW_NUMBER() OVER (PARTITION BY contract_address ORDER BY time DESC) AS latest
-from transfers)
+reserves as (
+    select
+        contract_address,
+        token0,
+        token1,
+        tx_hash,
+        block_time,
+        fee,
+        sum(transfer0) over (partition by contract_address order by block_time, evt_index) as reserve0,
+        sum(transfer1) over (partition by contract_address order by block_time, evt_index) as reserve1,
+        row_number() over (partition by tx_hash, contract_address order by evt_index desc) as row_num,
+        row_number() over (partition by contract_address order by block_time desc) as latest
+    from transfers
+)
 
 -- finds the TVL of the pools
-select r.contract_address, token0, token1, time, tx_hash,
-reserve0, reserve1,
-(reserve0 * p0.price / pow(10, p0.decimals)) + (reserve1 * p1.price / pow(10, p1.decimals)) as tvl,
-fee, latest
-from reserves r
+select
+    r.contract_address,
+    token0,
+    token1,
+    block_time,
+    tx_hash,
+    reserve0,
+    reserve1,
+    fee,
+    latest,
+    (reserve0 * p0.price / pow(10, p0.decimals)) + (reserve1 * p1.price / pow(10, p1.decimals)) as tvl
+from reserves as r
 inner join prices.usd as p0
-        on
-            date_trunc('minute', time) = p0.minute
-            and token0 = p0.contract_address
-    inner join prices.usd as p1
-        on
-            date_trunc('minute', time) = p1.minute
-            and token1 = p1.contract_address 
+    on
+        date_trunc('minute', block_time) = p0.minute
+        and token0 = p0.contract_address
+inner join prices.usd as p1
+    on
+        date_trunc('minute', block_time) = p1.minute
+        and token1 = p1.contract_address
 where row_num = 1
