d53d5c058e59bd9e2adcf44d2d5463d59a323e2f
pauljanicot@yahoo.com <pauljanicot@yahoo.com>
2024-11-22 15:38:23 +0000

    Added syncs inline to kpis.

--- cowamm/kpi/competitors/uni_swap_style/largest_uni_style_kpis_4304295.sql
+++ cowamm/kpi/competitors/uni_swap_style/largest_uni_style_kpis_4304295.sql
@@ -3,6 +3,7 @@
 -- The type of pool (Uni/Pancake/Sushi) is not specified
 -- Parameters:
 -- {{blockchain}}: The blockchain to query
+-- {{number_of_pools}}: The number of largest pools to return
 
 -- select the pool with the largest latest k
 with pool as (
@@ -12,6 +13,25 @@ with pool as (
         token1,
         tvl
     from "query_4303563(blockchain='{{blockchain}}', number_of_pools = '{{number_of_pools}}')"
+),
+
+syncs as (
+    select
+        pool.*,
+        tx_hash as evt_tx_hash,
+        index as evt_index,
+        block_time as evt_block_time,
+        block_number as evt_block_number,
+        varbinary_to_uint256(substr(data, 1, 32)) as reserve0,
+        varbinary_to_uint256(substr(data, 33, 32)) as reserve1,
+        rank() over (partition by (logs.contract_address) order by block_time desc) as latest
+    from {{blockchain}}.logs
+    inner join pool
+        on logs.contract_address = pool.contract_address
+    where
+        block_time >= date(date_add('day', -1, now()))
+        and topic0 = 0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1 -- Sync
+        
 ),
 
 swaps as (
@@ -27,27 +47,28 @@ swaps as (
         varbinary_to_uint256(substr(data, 97, 32)) as amount1Out
     from {{blockchain}}.logs
     where
-        topic0 = 0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822 -- Swap
+        block_time >= date(date_add('day', -1, now()))
+        and topic0 = 0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822 -- Swap
         and contract_address in (select contract_address from pool)
 ),
 
 -- gets the swapped volume and tvl at the time of the swap for each swap
 tvl_volume_per_swap as (
     select
-        syncs.pool_address,
+        syncs.contract_address,
         syncs.evt_block_time,
         syncs.evt_tx_hash,
         (amount0In * p0.price / pow(10, p0.decimals)) + (amount1In * p1.price / pow(10, p1.decimals)) as volume_in,
         (amount0Out * p0.price / pow(10, p0.decimals)) + (amount1Out * p1.price / pow(10, p1.decimals)) as volume_out,
         (reserve0 * p0.price / pow(10, p0.decimals)) + (reserve1 * p1.price / pow(10, p1.decimals)) as tvl
-    from "query_4304212(blockchain='{{blockchain}}', number_of_pools = '{{number_of_pools}}')" as syncs
+    from syncs
     inner join swaps
         on
             syncs.evt_tx_hash = swaps.evt_tx_hash
-            and syncs.pool_address = swaps.contract_address
+            and syncs.contract_address = swaps.contract_address
             and syncs.evt_index + 1 = swaps.evt_index
     inner join pool
-        on syncs.pool_address = pool.contract_address
+        on syncs.contract_address = pool.contract_address
     inner join prices.usd as p0
         on
             date_trunc('minute', syncs.evt_block_time) = p0.minute
@@ -56,13 +77,10 @@ tvl_volume_per_swap as (
         on
             date_trunc('minute', syncs.evt_block_time) = p1.minute
             and syncs.token1 = p1.contract_address
-    where
-        syncs.evt_block_time >= date(date_add('day', -1, now()))
-        and swaps.evt_block_time >= date(date_add('day', -1, now()))
 )
 
 select
-    contract_address,
+    pool.contract_address,
     0.003 as fee,
     coalesce(sum((volume_in + volume_out) / 2), 0) as volume,
     -- the average pool is conceptually unnecessary because the table pool has only one tvl per pool
@@ -71,5 +89,5 @@ select
     coalesce(365 * sum((volume_in + volume_out) / 2 / t.tvl) * 0.003, 0) as apr
 from pool
 left join tvl_volume_per_swap as t
-    on contract_address = pool_address
-group by contract_address
+    on pool.contract_address = t.contract_address
+group by pool.contract_address
