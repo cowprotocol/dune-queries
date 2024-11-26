f08a4b504ddd6b71eff5b64ed2cc63d80f309d43
pauljanicot@yahoo.com <pauljanicot@yahoo.com>
2024-11-26 17:30:08 +0000

    updated kpis to filter before joining and improve performance

--- cowamm/kpi/competitors/curve/curve_kpis_4232873.sql
+++ cowamm/kpi/competitors/curve/curve_kpis_4232873.sql
@@ -4,24 +4,24 @@
 -- {{blockchain}}: The blockchain to query
 -- {{competitor_end_time}}: The end time of the time window (end_time - 1 day; end_time), defaults to now()
 select
-    r1.contract_address,
-    r1.fee,
-    r1.tvl,
-    sum(amount_usd) as volume,
-    365 * sum(amount_usd * r.fee / r.tvl) as apr
+    contract_address,
+    fee,
+    tvl,
+    sum(amount_usd) over (partition by contract_address order by latest_per_pool) as volume,
+    365 * sum(amount_usd * fee / tvl) over (partition by contract_address order by latest_per_pool) as apr
 -- The first call to 4232976 gets the tvl after each tx to compute volume/tvl
 from "query_4232976(blockchain='{{blockchain}}')" as r
--- The second call to 4232976 gets only the last tvl and fee to display for the pool
-inner join "query_4232976(blockchain='{{blockchain}}')" as r1
-    on
-        r.contract_address = r1.contract_address
-left join curve.trades as t
+left join
+    ( --noqa: ST05
+        select *
+        from curve.trades
+        where
+            block_time >= date_add('day', -1, (case when '{{competitor_end_time}}' = '2100-01-01' then now() else timestamp '{{competitor_end_time}}' end))
+    ) as t
     on
         r.contract_address = t.project_contract_address
         and r.tx_hash = t.tx_hash
 where
-    t.block_time >= date_add('day', -1, (case when '{{competitor_end_time}}' = '2100-01-01' then now() else timestamp '{{competitor_end_time}}' end))
-    and r1.latest_per_pool = 1
+    latest_per_pool = 1
     -- This test avoids any possible issue with reconstructing the reserves of the pool
-    and r1.tvl > 0
-group by 1, 2, 3
+    and tvl > 0
