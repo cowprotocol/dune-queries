dc1c512f019eb65d2b1f485fd42ce698d286a792
PoloX2021 <115744849+PoloX2021@users.noreply.github.com>
2024-11-29 10:57:13 +0100

    CoW AMM monitoring (#72)
    * New queries to measure the kpis from competitors
    
    * new parameter added because of the previous queries
    
    * Excludes possible negative tvls because of issues with ERC20/ETH transfers
    
    * Formatting
    
    * Added a missing blockchain parameter
    
    * Simplified the querying of all tranactions from a curve pool
    
    * Few comments and adapting to remarks for Curve
    
    * Added filter by x largest pools
    
    * Added/propagated through queries number_of_pools parameter and various comments
    
    * Removed the union for inactive pools. THe left join does the work
    
    * Fixed the sqlfluff
    
    * Moved the folder to kpis/competitors
    
    * New query to replace the uni style querying. they are now grouped in the same query
    
    * Added the project value to classify the pools: uni/ pancake/sushi
    
    * Comments
    
    * Added parameter for time window, and removed avg(tvl), avg(fee)
    
    * upadated default parameter
    
    * Added syncs inline to kpis.
    
    * Added flag uni/pancake/Sushi and parameter for time window
    
    * New default value for day, few comments and renaming
    
    * Changed deployer contract addres for uniswap
    
    * Query to regroup all the different projects kpis together
    
    * Typo for chain_id
    
    Co-authored-by: Felix Leupold <felixleupold90@gmail.com>
    
    * fixed typo and added base
    
    * updated kpis to filter before joining and improve performance
    
    * increased number of pools to display
    
    * Adding CoW AMMs to the list of AMMs to monitor
    
    * typo in chain_id
    
    * Adding parameter for time window
    
    * wrong join leading to less pools visible
    
    * Adapting the query to get the info for every chain
    
    ---------
    
    Co-authored-by: Felix Leupold <felixleupold90@gmail.com>

--- cowamm/kpi/competitors/all_competitors_4335231.sql
+++ cowamm/kpi/competitors/all_competitors_4335231.sql
@@ -13,10 +13,10 @@ select
     case
         when '{{blockchain}}' = 'ethereum' then 1
         when '{{blockchain}}' = 'gnosis' then 100
-        when '{{blockchain}}' = 'arbitrum' then 8453
+        when '{{blockchain}}' = 'base' then 8453
         when '{{blockchain}}' = 'arbitrum' then 42161
     end as chain_id
-from "query_4304295(blockchain='{{blockchain}}')"
+from "query_4304295(blockchain='{{blockchain}}', competitor_end_time='{{competitor_end_time}}')"
 
 union distinct
 
@@ -31,7 +31,27 @@ select
     case
         when '{{blockchain}}' = 'ethereum' then 1
         when '{{blockchain}}' = 'gnosis' then 100
-        when '{{blockchain}}' = 'arbitrum' then 8453
+        when '{{blockchain}}' = 'base' then 8453
         when '{{blockchain}}' = 'arbitrum' then 42161
     end as chain_id
-from "query_4232873(blockchain='{{blockchain}}')"
+from "query_4232873(blockchain='ethereum', competitor_end_time='{{competitor_end_time}}')"
+-- there are no significant curve pools on arbitrum/gnosis
+where {{blockchain}} = 'ethereum'
+
+union distinct
+
+--CoW AMM
+select
+    contract_address,
+    tvl,
+    0 as fee,
+    coalesce(volume, 0) as volume,
+    coalesce(apr, 0) as apr,
+    'CoW AMM' as project,
+    case
+        when '{{blockchain}}' = 'ethereum' then 1
+        when '{{blockchain}}' = 'gnosis' then 100
+        when '{{blockchain}}' = 'base' then 8453
+        when '{{blockchain}}' = 'arbitrum' then 42161
+    end as chain_id
+from "query_4340428(blockchain='{{blockchain}}', competitor_end_time='{{competitor_end_time}}')"
