73f2cddb33fa8da3d814f9e5ce68198238eb402b
harisang <harisangelidakis@gmail.com>
2024-12-03 03:10:09 +0200

    increase accuracy

--- cowprotocol/raw_data/batch_data_4351957.sql
+++ cowprotocol/raw_data/batch_data_4351957.sql
@@ -11,14 +11,14 @@ past_batch_data_ethereum as (
         d.block_deadline,
         d.tx_hash,
         d.solver,
-        cast(d.data.execution_cost as decimal(32, 0)) as execution_cost, --noqa: RF01
-        cast(d.data.surplus as decimal(32, 0)) as surplus, --noqa: RF01
-        cast(d.data.protocol_fee as decimal(32, 0)) as protocol_fee, --noqa: RF01
-        cast(d.data.fee as decimal(32, 0)) as network_fee, --noqa: RF01
-        cast(d.data.uncapped_payment_eth as decimal(32, 0)) as uncapped_payment_native_token, --noqa: RF01
-        cast(d.data.capped_payment as decimal(32, 0)) as capped_payment, --noqa: RF01
-        cast(d.data.winning_score as decimal(32, 0)) as winning_score, --noqa: RF01
-        cast(d.data.reference_score as decimal(32, 0)) as reference_score --noqa: RF01
+        cast(d.data.execution_cost as decimal(38, 0)) as execution_cost, --noqa: RF01
+        cast(d.data.surplus as decimal(38, 0)) as surplus, --noqa: RF01
+        cast(d.data.protocol_fee as decimal(38, 0)) as protocol_fee, --noqa: RF01
+        cast(d.data.fee as decimal(38, 0)) as network_fee, --noqa: RF01
+        cast(d.data.uncapped_payment_eth as decimal(38, 0)) as uncapped_payment_native_token, --noqa: RF01
+        cast(d.data.capped_payment as decimal(38, 0)) as capped_payment, --noqa: RF01
+        cast(d.data.winning_score as decimal(38, 0)) as winning_score, --noqa: RF01
+        cast(d.data.reference_score as decimal(38, 0)) as reference_score --noqa: RF01
     from cowswap.raw_batch_rewards as d inner join cow_protocol_ethereum.solvers as s on d.solver = cast(s.address as varchar) where d.block_deadline < 20866925
 ),
 
@@ -67,14 +67,14 @@ select
     block_deadline,
     cast(tx_hash as varchar) as tx_hash,
     cast(solver as varchar) as solver,
-    cast(execution_cost as decimal(32, 0)) as execution_cost,
-    cast(surplus as decimal(32, 0)) as surplus,
-    cast(protocol_fee as decimal(32, 0)) as protocol_fee,
-    cast(network_fee as decimal(32, 0)) as network_fee,
-    cast(uncapped_payment_eth as decimal(32, 0)) as uncapped_payment_native_token,
-    cast(capped_payment as decimal(32, 0)) as capped_payment,
-    cast(winning_score as decimal(32, 0)) as winning_score,
-    cast(reference_score as decimal(32, 0)) as reference_score
+    cast(execution_cost as decimal(38, 0)) as execution_cost,
+    cast(surplus as decimal(38, 0)) as surplus,
+    cast(protocol_fee as decimal(38, 0)) as protocol_fee,
+    cast(network_fee as decimal(38, 0)) as network_fee,
+    cast(uncapped_payment_eth as decimal(38, 0)) as uncapped_payment_native_token,
+    cast(capped_payment as decimal(38, 0)) as capped_payment,
+    cast(winning_score as decimal(38, 0)) as winning_score,
+    cast(reference_score as decimal(38, 0)) as reference_score
 from dune.cowprotocol.dataset_batch_data_{{blockchain}}_2024_10
 union all
 select
@@ -84,14 +84,14 @@ select
     block_deadline,
     cast(tx_hash as varchar) as tx_hash,
     cast(solver as varchar) as solver,
-    cast(execution_cost as decimal(32, 0)) as execution_cost,
-    cast(surplus as decimal(32, 0)) as surplus,
-    cast(protocol_fee as decimal(32, 0)) as protocol_fee,
-    cast(network_fee as decimal(32, 0)) as network_fee,
-    cast(uncapped_payment_eth as decimal(32, 0)) as uncapped_payment_native_token,
-    cast(capped_payment as decimal(32, 0)) as capped_payment,
-    cast(winning_score as decimal(32, 0)) as winning_score,
-    cast(reference_score as decimal(32, 0)) as reference_score
+    cast(execution_cost as decimal(38, 0)) as execution_cost,
+    cast(surplus as decimal(38, 0)) as surplus,
+    cast(protocol_fee as decimal(38, 0)) as protocol_fee,
+    cast(network_fee as decimal(38, 0)) as network_fee,
+    cast(uncapped_payment_eth as decimal(38, 0)) as uncapped_payment_native_token,
+    cast(capped_payment as decimal(38, 0)) as capped_payment,
+    cast(winning_score as decimal(38, 0)) as winning_score,
+    cast(reference_score as decimal(38, 0)) as reference_score
 from dune.cowprotocol.dataset_batch_data_{{blockchain}}_2024_11
 union all
 select
@@ -101,14 +101,14 @@ select
     block_deadline,
     cast(tx_hash as varchar) as tx_hash,
     cast(solver as varchar) as solver,
-    cast(execution_cost as decimal(32, 0)) as execution_cost,
-    cast(surplus as decimal(32, 0)) as surplus,
-    cast(protocol_fee as decimal(32, 0)) as protocol_fee,
-    cast(network_fee as decimal(32, 0)) as network_fee,
-    cast(uncapped_payment_eth as decimal(32, 0)) as uncapped_payment_native_token,
-    cast(capped_payment as decimal(32, 0)) as capped_payment,
-    cast(winning_score as decimal(32, 0)) as winning_score,
-    cast(reference_score as decimal(32, 0)) as reference_score
+    cast(execution_cost as decimal(38, 0)) as execution_cost,
+    cast(surplus as decimal(38, 0)) as surplus,
+    cast(protocol_fee as decimal(38, 0)) as protocol_fee,
+    cast(network_fee as decimal(38, 0)) as network_fee,
+    cast(uncapped_payment_eth as decimal(38, 0)) as uncapped_payment_native_token,
+    cast(capped_payment as decimal(38, 0)) as capped_payment,
+    cast(winning_score as decimal(38, 0)) as winning_score,
+    cast(reference_score as decimal(38, 0)) as reference_score
 from dune.cowprotocol.dataset_batch_data_{{blockchain}}_2024_12
 union all
 select *
