6bcd092509d29f5b7ed283a3b4489d67ca6a61f7
Felix Leupold <felixleupold90@gmail.com>
2024-12-04 16:00:12 +0100

    rework averages

--- cowprotocol/execution_quality/aggregator_markout_4334277.sql
+++ cowprotocol/execution_quality/aggregator_markout_4334277.sql
@@ -5,8 +5,20 @@
 --  {{project}} - The aggregator to look at
 --  {{start}} - Start date for when trades should be counted
 --  {{blockchain}} - The chain on which trades should be counted
+--  {{top_n_pairs}} - Based on total DEX trading volume how many of the top token pairs to consider
 
-with trades as (
+with token_pairs as (
+    select
+        token_pair,
+        sum(amount_usd) as volume
+    from dex_aggregator.trades
+    where block_time >= timestamp '{{start}}'
+    group by 1
+    order by 2 desc
+    limit {{top_n_pairs}}
+),
+
+trades as (
     select
         block_time,
         amount_usd,
@@ -15,6 +27,8 @@ with trades as (
         (token_bought_amount / token_sold_amount) as project_price,
         p1.price / p0.price as dune_price
     from dex_aggregator.trades as p
+    inner join token_pairs
+        on p.token_pair = token_pairs.token_pair
     inner join prices.usd as p0
         on
             p0.contract_address = token_bought_address
@@ -25,7 +39,10 @@ with trades as (
             and p1.minute = date_trunc('minute', p.block_time)
     where
         block_time >= timestamp '{{start}}'
+        and amount_usd > 100
+        and token_bought_amount > 0
         and token_sold_amount > 0
+        and p0.price > 0
         and p1.price > 0
         and p.blockchain = '{{blockchain}}'
         and project = '{{project}}'
@@ -34,8 +51,7 @@ with trades as (
 select
     token_bought_symbol as buy_token,
     token_sold_symbol as sell_token,
-    -- geometric volume-weighted price ratio
-    exp(sum(amount_usd * ln(project_price / dune_price)) / sum(amount_usd)) as dune_price_ratio,
+    sum(amount_usd * project_price / dune_price) / sum(amount_usd) as dune_price_ratio,
     sum(amount_usd) as volume
 from trades
 group by 1, 2
--- cowprotocol/execution_quality/comparison_4334259.sql
+++ cowprotocol/execution_quality/comparison_4334259.sql
@@ -6,7 +6,7 @@
 --  {{project_b}} - The aggregator to compare with
 --  {{start}} - Start date for when trades should be counted
 --  {{blockchain}} - The chain on which trades should be counted
---  {{top_n_tokens}} - Based on trading volume (of project_a) how many of the top token pairs to consider
+--  {{top_n_pairs}} - Based on total DEX trading volume how many of the top token pairs to consider
 
 with comparison as (
     select
@@ -17,20 +17,17 @@ with comparison as (
         p2.dune_price_ratio as project_b_price_ratio,
         p2.volume as project_b_volume,
         p1.dune_price_ratio / p2.dune_price_ratio as project_a_advantage
-    from "query_4334277(start='{{start}}',blockchain='{{blockchain}}',project='{{project_a}}')" as p1
-    inner join "query_4334277(start='{{start}}',blockchain='{{blockchain}}',project='{{project_b}}')" as p2
+    from "query_4334277(start='{{start}}',blockchain='{{blockchain}}',project='{{project_a}}',top_n_pairs='{{top_n_pairs}}')" as p1
+    inner join "query_4334277(start='{{start}}',blockchain='{{blockchain}}',project='{{project_b}}',top_n_pairs='{{top_n_pairs}}')" as p2
         on
             p1.buy_token = p2.buy_token
             and p1.sell_token = p2.sell_token
     where
         p1.volume > 0
         and p2.volume > 0
-    order by p1.volume desc
-    limit {{top_n_tokens}}
 )
 
 select
-    -- geometric volume-weighted price ratio
-    exp(sum(project_b_volume * ln(project_a_advantage)) / sum(project_b_volume)) as avg_project_a_advantage,
+    (sum(project_a_volume * project_a_price_ratio) / sum(project_a_volume)) / (sum(project_b_volume * project_b_price_ratio) / sum(project_b_volume)) as avg_project_a_advantage,
     count(project_b_volume) as pairs
 from comparison
