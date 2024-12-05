02d705f35180d716c2070b7b55ac97f09d83ace9
harisang <harisangelidakis@gmail.com>
2024-12-05 17:01:43 +0200

    type changes and move to subfolder of accounting

--- cowprotocol/accounting/raw_data/order_data_4364122.sql
+++ cowprotocol/accounting/raw_data/order_data_4364122.sql
@@ -2,20 +2,21 @@
 -- for all auctions that had at least one winner.
 -- Parameters:
 --  {{blockchain}}: the chain for which we want to retrieve batch data
+
 with
 past_data_ethereum as (
     select
         s.environment,
         -1 as auction_id,
         d.block_number,
-        d.order_uid,
-        d.solver,
-        d.data.quote_solver, --noqa: RF01
-        d.tx_hash,
+        from_hex(d.order_uid) as order_uid,
+        from_hex(d.solver) as solver,
+        from_hex(d.data.quote_solver) as quote_solver, --noqa: RF01
+        from_hex(d.tx_hash) as tx_hash,
         cast(d.data.surplus_fee as decimal(38, 0)) as surplus_fee, --noqa: RF01
         cast(d.data.amount as decimal(38, 0)) as amount, --noqa: RF01
         cast(d.data.protocol_fee as decimal(38, 0)) as protocol_fee, --noqa: RF01
-        d.data.protocol_fee_token, --noqa: RF01
+        from_hex(d.data.protocol_fee_token) as protocol_fee_token, --noqa: RF01
         cast(d.data.protocol_fee_native_price as decimal(38, 0)) as protocol_fee_native_price, --noqa: RF01
         case
             when d.data.quote_sell_amount = 'None' then null --noqa: RF01
@@ -34,9 +35,9 @@ past_data_ethereum as (
             else cast(d.data.quote_sell_token_price as decimal(38, 0)) --noqa: RF01
         end as quote_sell_token_price,
         cast(d.data.partner_fee as decimal(38, 0)) as partner_fee, --noqa: RF01
-        d.data.partner_fee_recipient, --noqa: RF01
+        from_hex(d.data.partner_fee_recipient) as partner_fee_recipient, --noqa: RF01
         d.data.protocol_fee_kind --noqa: RF01
-    from cowswap.raw_order_rewards as d inner join cow_protocol_ethereum.solvers as s on cast(d.solver as varchar) = cast(s.address as varchar) where d.block_number < 20866925
+    from cowswap.raw_order_rewards as d inner join cow_protocol_ethereum.solvers as s on from_hex(d.solver) = s.address where d.block_number < 20866925
 ),
 
 past_data_gnosis as ( --noqa: ST03
@@ -45,14 +46,14 @@ past_data_gnosis as ( --noqa: ST03
         s.environment,
         -1 as auction_id,
         d.block_number,
-        cast(d.order_uid as varchar) as order_uid,
-        cast(d.solver as varchar) as solver,
-        cast(d.quote_solver as varchar) as quote_solver,
-        cast(d.tx_hash as varchar) as tx_hash,
+        d.order_uid,
+        d.solver,
+        d.quote_solver,
+        d.tx_hash,
         cast(d.surplus_fee as decimal(38, 0)) as surplus_fee,
         cast(d.amount as decimal(38, 0)) as amount,
         cast(d.protocol_fee as decimal(38, 0)) as protocol_fee,
-        cast(d.protocol_fee_token as varchar) as protocol_fee_token,
+        d.protocol_fee_token,
         cast(d.protocol_fee_native_price as decimal(38, 0)) as protocol_fee_native_price,
         cast(d.quote_sell_amount as decimal(38, 0)) as quote_sell_amount,
         cast(d.quote_buy_amount as decimal(38, 0)) as quote_buy_amount,
@@ -65,7 +66,7 @@ past_data_gnosis as ( --noqa: ST03
             else cast(d.quote_sell_token_price as decimal(38, 0))
         end as quote_sell_token_price,
         cast(d.partner_fee as decimal(38, 0)) as partner_fee,
-        cast(d.partner_fee_recipient as varchar) as partner_fee_recipient,
+        d.partner_fee_recipient,
         d.protocol_fee_kind
     from dune.cowprotocol.dataset_cowswap_gnosis_raw_order_rewards as d inner join cow_protocol_gnosis.solvers as s on cast(d.solver as varchar) = cast(s.address as varchar)
 ),
@@ -76,14 +77,14 @@ past_data_arbitrum as ( --noqa: ST03
         s.environment,
         -1 as auction_id,
         d.block_number,
-        cast(d.order_uid as varchar) as order_uid,
-        cast(d.solver as varchar) as solver,
-        cast(d.quote_solver as varchar) as quote_solver,
-        cast(d.tx_hash as varchar) as tx_hash,
+        d.order_uid,
+        d.solver,
+        d.quote_solver,
+        d.tx_hash,
         cast(d.surplus_fee as decimal(38, 0)) as surplus_fee,
         cast(d.amount as decimal(38, 0)) as amount,
         cast(d.protocol_fee as decimal(38, 0)) as protocol_fee,
-        cast(d.protocol_fee_token as varchar) as protocol_fee_token,
+        d.protocol_fee_token,
         case
             when d.protocol_fee_native_price = 'inf' then null
             else cast(d.protocol_fee_native_price as decimal(38, 0))
@@ -99,7 +100,7 @@ past_data_arbitrum as ( --noqa: ST03
             else cast(d.quote_sell_token_price as decimal(38, 0))
         end as quote_sell_token_price,
         cast(d.partner_fee as decimal(38, 0)) as partner_fee,
-        cast(d.partner_fee_recipient as varchar) as partner_fee_recipient,
+        d.partner_fee_recipient,
         d.protocol_fee_kind
     from dune.cowprotocol.dataset_cowswap_arbitrum_raw_order_rewards as d inner join cow_protocol_arbitrum.solvers as s on cast(d.solver as varchar) = cast(s.address as varchar)
 )
@@ -108,14 +109,14 @@ select
     environment,
     auction_id,
     block_number,
-    cast(order_uid as varchar) as order_uid,
-    cast(solver as varchar) as solver,
-    cast(quote_solver as varchar) as quote_solver,
-    cast(tx_hash as varchar) as tx_hash,
+    order_uid,
+    solver,
+    quote_solver,
+    tx_hash,
     cast(surplus_fee as decimal(38, 0)) as surplus_fee,
     cast(amount as decimal(38, 0)) as amount,
     cast(protocol_fee as decimal(38, 0)) as protocol_fee,
-    cast(protocol_fee_token as varchar) as protocol_fee_token,
+    protocol_fee_token,
     cast(protocol_fee_native_price as decimal(38, 0)) as protocol_fee_native_price,
     cast(quote_sell_amount as decimal(38, 0)) as quote_sell_amount,
     cast(quote_buy_amount as decimal(38, 0)) as quote_buy_amount,
@@ -125,7 +126,7 @@ select
         else cast(quote_sell_token_price as decimal(38, 0))
     end as quote_sell_token_price,
     cast(partner_fee as decimal(38, 0)) as partner_fee,
-    cast(partner_fee_recipient as varchar) as partner_fee_recipient,
+    partner_fee_recipient,
     protocol_fee_kind
 from dune.cowprotocol.dataset_order_data_{{blockchain}}_2024_10
 union all
@@ -133,14 +134,14 @@ select
     environment,
     auction_id,
     block_number,
-    cast(order_uid as varchar) as order_uid,
-    cast(solver as varchar) as solver,
-    cast(quote_solver as varchar) as quote_solver,
-    cast(tx_hash as varchar) as tx_hash,
+    order_uid,
+    solver,
+    quote_solver,
+    tx_hash,
     cast(surplus_fee as decimal(38, 0)) as surplus_fee,
     cast(amount as decimal(38, 0)) as amount,
     cast(protocol_fee as decimal(38, 0)) as protocol_fee,
-    cast(protocol_fee_token as varchar) as protocol_fee_token,
+    protocol_fee_token,
     cast(protocol_fee_native_price as decimal(38, 0)) as protocol_fee_native_price,
     cast(quote_sell_amount as decimal(38, 0)) as quote_sell_amount,
     cast(quote_buy_amount as decimal(38, 0)) as quote_buy_amount,
@@ -150,7 +151,7 @@ select
         else cast(quote_sell_token_price as decimal(38, 0))
     end as quote_sell_token_price,
     cast(partner_fee as decimal(38, 0)) as partner_fee,
-    cast(partner_fee_recipient as varchar) as partner_fee_recipient,
+    partner_fee_recipient,
     protocol_fee_kind
 from dune.cowprotocol.dataset_order_data_{{blockchain}}_2024_11
 union all
@@ -158,21 +159,21 @@ select
     environment,
     auction_id,
     block_number,
-    cast(order_uid as varchar) as order_uid,
-    cast(solver as varchar) as solver,
-    cast(quote_solver as varchar) as quote_solver,
-    cast(tx_hash as varchar) as tx_hash,
+    order_uid,
+    solver,
+    quote_solver,
+    tx_hash,
     cast(surplus_fee as decimal(38, 0)) as surplus_fee,
     cast(amount as decimal(38, 0)) as amount,
     cast(protocol_fee as decimal(38, 0)) as protocol_fee,
-    cast(protocol_fee_token as varchar) as protocol_fee_token,
+    protocol_fee_token,
     cast(protocol_fee_native_price as decimal(38, 0)) as protocol_fee_native_price,
     cast(quote_sell_amount as decimal(38, 0)) as quote_sell_amount,
     cast(quote_buy_amount as decimal(38, 0)) as quote_buy_amount,
     cast(quote_gas_cost as decimal(38, 0)) as quote_gas_cost,
     cast(quote_sell_token_price as decimal(38, 0)) as quote_sell_token_price,
     cast(partner_fee as decimal(38, 0)) as partner_fee,
-    cast(partner_fee_recipient as varchar) as partner_fee_recipient,
+    partner_fee_recipient,
     protocol_fee_kind
 from dune.cowprotocol.dataset_order_data_{{blockchain}}_2024_12
 union all
