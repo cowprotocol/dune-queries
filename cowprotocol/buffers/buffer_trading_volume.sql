b7200f84008d405f855231a39922a2e1493bfda2
pauljanicot@yahoo.com <pauljanicot@yahoo.com>
2024-12-13 12:05:36 +0000

    Commenting the code

--- cowprotocol/buffers/buffer_trading_volume.sql
+++ cowprotocol/buffers/buffer_trading_volume.sql
@@ -1,12 +1,19 @@
+-- Computes the trading volume of the buffers
+-- The heuristic is:
+-- For each transaction, we account for all the transfers through the settlement contract
+-- If a token comes in but never comes out or reciprocally it is considered as volume
+
 -- Parameters
 --  {{blockchain}} - chain for which the query is running
---  {{start_time}}
---  {{end_time}}
+--  {{start_time}}: rounded to a date
+--  {{end_time}}: rounded to a date
+--      the period is date start_time <= start_time <= date end_time
 
 with transfers as (
     select
         contract_address,
         evt_block_time,
+        -- net_inflow is the net value (positive if in, negative if out) for each token which comes in the GPV2Settlement contract
         sum(case when to = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41 then value else -value end) as net_inflow
     from erc20_{{blockchain}}.evt_transfer
     where
@@ -26,4 +33,7 @@ inner join transfers as t
     on
         p.contract_address = t.contract_address
         and timestamp = date_trunc('minute', evt_block_time)
-where not (net_inflow = 0)
+--improves slightly efficiency as it reduces the number of rows
+where
+    not (net_inflow = 0)
+    and timestamp between (date '{{start_time}}') and (date '{{end_time}}')
