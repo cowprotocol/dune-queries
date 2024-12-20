d785d18e4b58e4bf0dc03beeea3ce53aaeac057f
harisang <harisangelidakis@gmail.com>
2024-12-20 13:24:41 +0200

    rename columns

--- cowprotocol/accounting/fees/protocol_fees_by_order_class_3703312.sql
+++ cowprotocol/accounting/fees/protocol_fees_by_order_class_3703312.sql
@@ -1,5 +1,6 @@
 -- This query computes protocol fees collected by the DAO,
 -- and breaks down the revenue based on order class.
+-- Native token is ETH, with the exception of Gnosis Chain, where it is xDAI
 --
 -- Parameters:
 --  {{start_time}} - the timestamp for which the analysis should start (inclusively)
@@ -22,12 +23,12 @@ initial_block as (
 protocol_fees_collected as (
     select --noqa: ST06
         order_uid,
-        protocol_fee * protocol_fee_native_price / pow(10, 18) as protocol_fee_in_eth,
+        protocol_fee * protocol_fee_native_price / pow(10, 18) as protocol_fee_in_native_token,
         protocol_fee as protocol_fee_in_surplus_token,
         case
             when partner_fee_recipient is not null then partner_fee * protocol_fee_native_price / pow(10, 18)
-        end as partner_fee_eth,
-        protocol_fee * protocol_fee_native_price / pow(10, 18) - coalesce(case when partner_fee_recipient is not null then cast(partner_fee as int256) * protocol_fee_native_price / pow(10, 18) end, 0) as net_protocol_fee_in_eth,
+        end as partner_fee_native_token,
+        protocol_fee * protocol_fee_native_price / pow(10, 18) - coalesce(case when partner_fee_recipient is not null then cast(partner_fee as int256) * protocol_fee_native_price / pow(10, 18) end, 0) as net_protocol_fee_in_native_token,
         protocol_fee_token as surplus_token,
         quote_gas_cost,
         quote_sell_token_price,
@@ -40,16 +41,16 @@ protocol_fees_collected as (
 
 select
     order_class,
-    sum(protocol_fee_in_eth) as total_fee_in_eth,
-    sum(net_protocol_fee_in_eth) as net_protocol_fee_in_eth,
-    sum(partner_fee_eth) as total_partner_fee,
+    sum(protocol_fee_in_native_token) as total_fee_in_native_token,
+    sum(net_protocol_fee_in_native_token) as net_protocol_fee_in_native_token,
+    sum(partner_fee_native_token) as total_partner_fee,
     sum(case
-        when partner_recipient = '0x63695Eee2c3141BDE314C5a6f89B98E62808d716' then partner_fee_eth * 0.90
-        when partner_recipient is not null then partner_fee_eth * 0.85
+        when partner_recipient = '0x63695Eee2c3141BDE314C5a6f89B98E62808d716' then partner_fee_native_token * 0.90
+        when partner_recipient is not null then partner_fee_native_token * 0.85
     end) as partner_fee_part,
     sum(case
-        when partner_recipient = '0x63695Eee2c3141BDE314C5a6f89B98E62808d716' then partner_fee_eth * 0.10
-        when partner_recipient is not null then partner_fee_eth * 0.15
+        when partner_recipient = '0x63695Eee2c3141BDE314C5a6f89B98E62808d716' then partner_fee_native_token * 0.10
+        when partner_recipient is not null then partner_fee_native_token * 0.15
     end) as cow_dao_partner_fee_part
 from protocol_fees_collected as f
 inner join cow_protocol_{{blockchain}}.trades as t
