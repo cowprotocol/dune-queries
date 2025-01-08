4cae9c1b3c631e8462657b0869ba7cf42318fec5
Felix Leupold <felixleupold90@gmail.com>
2025-01-08 11:16:06 +0100

    [Revenue] Add Revenue by solver/integrator per chain (#109)
    * [Revenue] Add monthly DAO revenue query
    
    * some more comments for readability
    
    * top level query comments
    
    * [Revenue] Per Chain Queries
    
    * fix lint
    
    * exclude mispriced trades
    
    * some bugfixes
    
    * [Revenue] Add Reveny by solver/integrator per chain

---
+++ cowprotocol/revenue/daily_revenue_by_integration_per_chain_4515022.sql
@@ -0,0 +1,13 @@
+-- This query returns the daily revenue grouped by integration for a specific target chain
+-- Parameters:
+--  {{ui_fee_recipient}} - the partner address that receives the CoW Swap UI fee
+--  {{blockchain}} - the chain for which to collect the data
+
+select
+    s.name,
+    date(block_time) as "day",
+    coalesce(sum("Limit"), 0) + coalesce(sum("Market"), 0) + coalesce(sum("UI Fee"), 0) + coalesce(sum("Partner Fee Share"), 0) as total
+from "query_4217030(blockchain='{{blockchain}}',ui_fee_recipient='{{ui_fee_recipient}}')" as r
+left join cow_protocol_{{blockchain}}.solvers as s
+    on r.solver = s.address
+group by 1, 2
