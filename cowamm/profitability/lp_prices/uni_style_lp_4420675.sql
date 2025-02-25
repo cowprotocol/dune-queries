60f88d1d5b59ced66fb1d57066c6af77543207c3
PoloX2021 <pauljanicot@yahoo.com>
2025-02-25 14:27:17 +0000

    Query to get the balances and reserves of uni style pools

--- cowamm/profitability/lp_prices/uni_style_lp_4420675.sql
+++ cowamm/profitability/lp_prices/uni_style_lp_4420675.sql
@@ -1,4 +1,4 @@
--- This query gets the reserve of every Univ2 style pool at the end of a day 
+-- This query gets the reserve of every Univ2 style pool at the beginning of every day 
 -- and the lp token transfers
 --Parameters
 --  {{blockchain}}: The blockchain to query
@@ -6,7 +6,32 @@
 --  {{end}}: The end date of the analysis. date(Timestamp) <= date(timestamp '{{end}}').
 --      For a 1 day period, {{end}} = {{start}}
 
-with date_range as (
+--
+
+with cow_amm_pools as (
+    select * from dune.cowprotocol.result_balancer_co_w_am_ms
+),
+
+--get all the the pools derived from uniswap which compare to cow amms
+uni_style_pools as(
+    select
+        u.created_at,
+        u.contract_address,
+        u.token0,
+        u.token1,
+        u.project,
+        --cow amms can have multiple pools for a same pair
+        min(c.created_at) as cow_created_at
+    from "query_4420646(blockchain = 'ethereum')" as u
+    inner join cow_amm_pools as c
+        on 
+            (u.token0 = c.token_1_address and u.token1 = c.token_2_address)
+            or (u.token1 = c.token_1_address and u.token0 = c.token_2_address)
+    group by 1, 2, 3, 4, 5
+),
+
+--time frame of the analysis
+date_range as (
     select t.day
     from
         unnest(sequence(
@@ -15,79 +40,73 @@ with date_range as (
         )) t (day) --noqa: AL01
 ),
 
-pools as (
-    select *
-    from "query_4420646(blockchain='{{blockchain}}')"
-),
-
 lp_balance_delta as (
     select
-        contract_address,
+        evt_transfer.contract_address,
         date(evt_block_time) as "day",
         sum(case when "from" = 0x0000000000000000000000000000000000000000 then value else -value end) as lp_transfer
     from erc20_{{blockchain}}.evt_transfer
+    inner join uni_style_pools as u
+        on u.contract_address = evt_transfer.contract_address
     where
-        contract_address in (select contract_address from pools)
-        and ("from" = 0x0000000000000000000000000000000000000000 or "to" = 0x0000000000000000000000000000000000000000)
-        and evt_block_time >= date(timestamp '{{start}}')
-        and date(evt_block_time) <= date(timestamp '{{end}}')
+        ("from" = 0x0000000000000000000000000000000000000000 or "to" = 0x0000000000000000000000000000000000000000)
+        and evt_block_time >= date(u.created_at)
     group by 1, 2
 ),
 
 syncs as (
     select
-        pools.contract_address,
+        u.contract_address,
         date_trunc('day', block_time) as "day",
         varbinary_to_uint256(substr(data, 1, 32)) as reserve0,
         varbinary_to_uint256(substr(data, 33, 32)) as reserve1,
-        rank() over (partition by (pools.contract_address, date_trunc('day', block_time)) order by block_time desc, index desc) as latest
+        rank() over (partition by (logs.contract_address, date_trunc('day', block_time)) order by block_time desc, index desc) as latest
     from {{blockchain}}.logs
-    inner join pools
-        on logs.contract_address = pools.contract_address
+    inner join uni_style_pools as u
+        on logs.contract_address = u.contract_address
     where
         topic0 = 0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1 -- Sync
-        and logs.block_time >= date(timestamp '{{start}}')
-        and date(logs.block_time) <= date(timestamp '{{end}}')
+        and logs.block_time >= date(u.created_at)
 )
 
-select
-    p.contract_address,
-    project,
-    token0,
-    token1,
-    d.day,
-    price0.price as price0,
-    price1.price as price1,
-    price0.decimals as decimals0,
-    price1.decimals as decimals1,
-    coalesce(l.lp_transfer, 0) as lp_transfer,
-    --fill in the days without reserves
-    last_value(s.reserve0) ignore nulls over (partition by p.contract_address order by d.day range between unbounded preceding and current row) as reserve0,
-    last_value(s.reserve1) ignore nulls over (partition by p.contract_address order by d.day range between unbounded preceding and current row) as reserve1
-from date_range as d
-cross join pools as p
-left join lp_balance_delta as l
-    on
-        d.day = l.day
-        and p.contract_address = l.contract_address
---only keep the last reserves of the day
-left join (--noqa: ST05
-    select *
-    from syncs
-    where latest = 1
-) as s
-    on
-        d.day = s.day
-        and p.contract_address = s.contract_address
-left join prices.day as price0
-    on
-        d.day = price0.timestamp
-        and p.token0 = price0.contract_address
-left join prices.day as price1
-    on
-        d.day = price1.timestamp
-        and p.token1 = price1.contract_address
-where
-    (price0.blockchain = '{{blockchain}}' or price0.blockchain is null)
-    and (price1.blockchain = '{{blockchain}}' or price0.blockchain is null)
-    and d.day >= date_trunc('day', p.created_at)
+select *
+from (
+    select 
+        u.created_at,
+        u.cow_created_at,
+        u.contract_address,
+        d.day,
+        u.token0,
+        u.token1,
+        u.project,
+        sum(coalesce(l.lp_transfer, 0)) over (
+            partition by u.contract_address
+            order by d.day asc
+            rows between unbounded preceding and 1 preceding
+        ) as lp_reserve,
+        coalesce(
+            last_value(s.reserve0) over (
+                partition by s.contract_address 
+                order by s.day asc 
+                rows between unbounded preceding and 1 preceding),
+            0
+        ) as reserve0,
+        coalesce(
+            last_value(s.reserve1) over (
+                partition by s.contract_address 
+                order by s.day asc 
+                rows between unbounded preceding and 1 preceding),
+            0
+        ) as reserve1
+    from uni_style_pools as u
+    cross join date_range as d
+    left join lp_balance_delta as l
+        on 
+            u.contract_address = l.contract_address
+            and d.day = l.day
+    left join (select * from syncs where latest = 1) as s
+        on 
+            u.contract_address = s.contract_address
+            and d.day = s.day
+)
+where day >= created_at
