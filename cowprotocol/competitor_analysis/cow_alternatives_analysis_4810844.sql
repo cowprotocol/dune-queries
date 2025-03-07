9bf4f97fadbb110eeb545137c9d06ffff946828f
a1exandr0 <sanek07032000@gmail.com>
2025-03-07 16:30:29 +0100

    fix aggregation

--- cowprotocol/competitor_analysis/cow_alternatives_analysis_4810844.sql
+++ cowprotocol/competitor_analysis/cow_alternatives_analysis_4810844.sql
@@ -81,22 +81,14 @@ all_competitor_transactions as (
 ),
 
 joined_and_filtered as (
-    select *
+    select
+        *,
+        count(distinct address) over (partition by chain_used_for_competitor) as distinct_users_count_per_chain
     from
         cow_users_filtered
     inner join all_competitor_transactions using (address) -- noqa: disable=L032
     where
         {{show_competitors_on_target_chain_only}} = 0 or chain_used_for_competitor = '{{chain_of_interest}}'
-),
-
-user_count_per_chain as (
-    select
-        chain_used_for_competitor,
-        count(distinct address) as distinct_users_count_per_chain
-    from
-        joined_and_filtered
-    group by
-        1
 ),
 
 aggregated as (
@@ -105,7 +97,8 @@ aggregated as (
         competitor_project,
         sum(competitor_total_volume_usd) as competitor_total_volume_usd,
         sum(competitor_total_transactions) as competitor_total_transactions,
-        count(distinct address) as distinct_users_count
+        count(distinct address) as distinct_users_count_per_product_per_chain,
+        any_value(distinct_users_count_per_chain) as distinct_users_count_per_chain
     from
         joined_and_filtered
     group by 1, 2
@@ -117,9 +110,8 @@ select
     competitor_project,
     competitor_total_volume_usd,
     competitor_total_transactions,
-    distinct_users_count as distinct_users_count_per_product_per_chain,
+    distinct_users_count_per_product_per_chain,
     distinct_users_count_per_chain
 from
     aggregated
-left join user_count_per_chain using (chain_used_for_competitor) -- noqa: disable=L032
 order by 3 desc
