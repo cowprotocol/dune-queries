1a6df7251ef7b7aa9f3abbdafea3964eab1036b1
PoloX2021 <pauljanicot@yahoo.com>
2025-03-13 22:23:45 +0000

    Adding volume queries from dashboard

---
+++ cowamm/dashboard/new_dashboard/total_volume_3968105.sql
@@ -0,0 +1,27 @@
+--Query computes the trading volume over all Cow AMMs (matching users and rebalancing) and 7 day growth thereof.
+with cow_trades as (
+    select
+        block_date,
+        sum(amount_usd) as volume
+    from cow_protocol.trades as t
+    inner join dune.cowprotocol.result_balancer_co_w_am_ms as pool
+        on t.taker = pool.address
+    group by 1
+),
+
+cumulative_volume as (
+    select
+        block_date as day, --noqa: RF04
+        sum(volume) over (order by block_date) as tvl
+    from cow_trades
+)
+
+select
+    prev.tvl as prev,
+    curr.tvl as curr,
+    100 * (curr.tvl - prev.tvl) / prev.tvl as growth
+from cumulative_volume as curr
+inner join cumulative_volume as prev
+    on curr.day = prev.day + interval '7' day
+    -- we don't have data for today
+    and curr.day = date(now()) - interval '1' day
