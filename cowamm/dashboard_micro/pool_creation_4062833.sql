d83b9ae90f50f48873e6e622920a2bf663969696
PoloX2021 <pauljanicot@yahoo.com>
2025-03-13 11:48:39 +0000

    Add an updated version of the query. Test query id:  4844042

---
+++ cowamm/dashboard_micro/pool_creation_4062833.sql
@@ -0,0 +1,13 @@
+--display the informations about a cow amm pool
+-- Parameters:
+--   contract_address: the address of the pool
+
+select
+    date(created_at) as created_day,
+    concat('<a href="https://etherscan.io/address/', cast(contract_address as varchar), '" target="_blank">', cast(contract_address as varchar), '</a>') as pool_address,
+    concat('<a href="https://etherscan.io/address/', cast(token0 as varchar), '" target="_blank">', symbol0, ' ', cast(weight0 as varchar), '%', '</a>') as symbol0,
+    concat('<a href="https://etherscan.io/address/', cast(token1 as varchar), '" target="_blank">', symbol1, ' ', cast(weight1 as varchar), '%', '</a>') as symbol1
+from dune.cowprotocol.result_amm_lp_infos
+where (contract_address = {{cow_amm}})
+order by 1 desc
+limit 1
