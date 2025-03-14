26aefbf8b4bd702d30e1daf3049ec6fc6a04c41f
PoloX2021 <pauljanicot@yahoo.com>
2025-03-14 12:28:08 +0000

    Update dollar surplus formula

--- cowamm/dashboard_micro/cow_amm_volume_invariant_tvl_4077968.sql
+++ cowamm/dashboard_micro/cow_amm_volume_invariant_tvl_4077968.sql
@@ -6,7 +6,7 @@ with volume as (
     select
         block_date,
         sum(usd_value) as volume
-    from cow_protocol_ethereum.trades
+    from cow_protocol_{{blockchain}}.trades
     where
         block_date >= timestamp '{{start}}'
         and trader = {{cow_amm}}
@@ -14,16 +14,18 @@ with volume as (
 
 select
     day,
-    tvl,
+    value0 + value1 as tvl,
     volume,
     -- compute the invariant growth per LP token
-    (power(value0, weight0) * power(value1, weight1) / lp_reserve
-    - lag(power(reserve0, weight0) * power(reserve1, weight1) / lp_reserve) over (order by day))
-    -- then get the total invariant growth and price it in $$
-    * lp_reserve * price0 * power(10, -decimals0 * weight0) * price1 * power(10, -decimals1 * weight1) as surplus
+    (power(reserve0, cast(weight0 as double) / 100) * power(reserve1, cast(weight1 as double) / 100) / lp_reserve
+    - lag(power(reserve0, cast(weight0 as double) / 100) * power(reserve1, cast(weight1 as double) / 100) / lp_reserve) over (order by day))
+    -- then get the total invariant growth and price it in $$ and weight it with the same as the pool
+    * lp_reserve * power(price0 * power(10, -decimals0), cast(weight0 as double) / 100) * power(price1 * power(10, -decimals1), cast(weight1 as double) / 100) as surplus
 from dune.cowprotocol.result_amm_lp_infos
-inner join volume
+left join volume
     on
         day = block_date
-        and contract_address = {{cow_amm}}
-where day >= timestamp '{{start}}'
+where
+    day >= timestamp '{{start}}'
+    and contract_address = {{cow_amm}}
+
