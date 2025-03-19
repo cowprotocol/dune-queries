84a6c048d717cd6b1e4b71a3910a2f35dbfc8c5e
PoloX2021 <115744849+PoloX2021@users.noreply.github.com>
2025-03-19 22:51:57 +0000

    Update cowamm/dashboard_micro/cow_amm_current_comparison_4059465.sql
    Co-authored-by: Haris Angelidakis <64154020+harisang@users.noreply.github.com>

--- cowamm/dashboard_micro/cow_amm_current_comparison_4059465.sql
+++ cowamm/dashboard_micro/cow_amm_current_comparison_4059465.sql
@@ -1,6 +1,6 @@
 --Current outperformance of COW AMM vs Uniswap and Sushiswap and TVL
 -- Parameters:
--- cow_amm: COW AMM address
+-- cow_amm: CoW AMM address
 -- start: Start date for the query
 
 with comparison as (
