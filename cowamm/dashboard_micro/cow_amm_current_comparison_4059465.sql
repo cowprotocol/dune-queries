684e81b0e23f261cf49c710ffb99cfefb6967775
PoloX2021 <pauljanicot@yahoo.com>
2025-03-31 05:38:37 +0100

    Add monthly edge

--- cowamm/dashboard_micro/cow_amm_current_comparison_4059465.sql
+++ cowamm/dashboard_micro/cow_amm_current_comparison_4059465.sql
@@ -21,9 +21,16 @@ select
     100 * ((c.cow_10k / c7.cow_10k / (c.uni_10k / c7.uni_10k)) - 1) as over_uni_return_1w,
     100 * ((c.cow_10k / c7.cow_10k / (c.sushi_10k / c7.sushi_10k)) - 1) as over_sushi_return_1w,
     100 * ((c.cow_10k / c7.cow_10k / (c.pancake_10k / c7.pancake_10k)) - 1) as over_pancake_return_1w,
-    100 * ((c.cow_10k / c7.cow_10k / (c.rebalance_10k / c7.rebalance_10k)) - 1) as over_reb_return_1w
+    100 * ((c.cow_10k / c7.cow_10k / (c.rebalance_10k / c7.rebalance_10k)) - 1) as over_reb_return_1w,
+    -- 30 day growth
+    100 * ((c.cow_10k / c30.cow_10k / (c.uni_10k / c30.uni_10k)) - 1) as over_uni_return_1m,
+    100 * ((c.cow_10k / c30.cow_10k / (c.sushi_10k / c30.sushi_10k)) - 1) as over_sushi_return_1m,
+    100 * ((c.cow_10k / c30.cow_10k / (c.pancake_10k / c30.pancake_10k)) - 1) as over_pancake_return_1m,
+    100 * ((c.cow_10k / c30.cow_10k / (c.rebalance_10k / c30.rebalance_10k)) - 1) as over_reb_return_1m
 from comparison as c
 inner join comparison as c7
     on c.day = c7.day + interval '7' day
+inner join comparison as c30
+    on c.day = c30.day + interval '30' day
 order by c.day desc
 limit 1
