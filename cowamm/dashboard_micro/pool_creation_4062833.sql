ab7de3a2b81a24a6a50ef8d8a10a594234576f86
PoloX2021 <115744849+PoloX2021@users.noreply.github.com>
2025-05-06 06:42:08 +0100

    Cow amm dashboards update (#160)
    * Reorganizing the folders
    
    * Wrong variable name
    
    * Add tvl growth query
    
    * Merge 2 queries in 1 for credit efficiency
    
    * Adding non null condition
    
    * Format for dashboard
    
    * update the link to the micro dashboard
    
    * Linting
    
    * rounding for visualization purposes
    
    * Multiply by 100 for visualization
    
    * Add TVL
    
    * Linting
    
    * lint
    
    * Annualize the apy
    
    * Add monthly edge

--- cowamm/dashboard_micro/pool_creation_4062833.sql
+++ cowamm/dashboard_micro/pool_creation_4062833.sql
@@ -4,10 +4,11 @@
 
 select
     date(created_at) as created_day,
+    value0 + value1 as tvl,
     concat('<a href="https://etherscan.io/address/', cast(contract_address as varchar), '" target="_blank">', cast(contract_address as varchar), '</a>') as pool_address,
     concat('<a href="https://etherscan.io/address/', cast(token0 as varchar), '" target="_blank">', symbol0, ' ', cast(weight0 as varchar), '%', '</a>') as symbol0,
     concat('<a href="https://etherscan.io/address/', cast(token1 as varchar), '" target="_blank">', symbol1, ' ', cast(weight1 as varchar), '%', '</a>') as symbol1
 from dune.cowprotocol.result_amm_lp_infos
 where (contract_address = {{cow_amm}})
-order by 1 desc
+order by created_at desc, day desc
 limit 1
