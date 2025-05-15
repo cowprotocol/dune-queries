01230a4a61263bf25ebba81c65ce7aa0b6e24899
harisang <harisangelidakis@gmail.com>
2025-05-15 10:18:43 +0300

    fix linting

--- cowprotocol/solver_dashboard/ranked_active_solvers_1756858.sql
+++ cowprotocol/solver_dashboard/ranked_active_solvers_1756858.sql
@@ -1,61 +1,61 @@
 -- This query provides various metrics to compare the different operating solvers.
 -- It looks back over a period of LastNDays Days
 -- Parameters:
---   {{last_n_days}}: int the number of days to look back
+--   {{last_n_days}}: int, corresponding to the number of days to look back
 
-WITH settled_batches AS (
-    SELECT DISTINCT tx_hash
-    FROM cow_protocol_{{blockchain}}.trades
-    WHERE block_date > now() - interval '{{last_n_days}}' day
+with settled_batches as (
+    select distinct tx_hash
+    from cow_protocol_{{blockchain}}.trades
+    where block_date > now() - interval '{{last_n_days}}' day
 ),
 
-trade_agg AS (
-    SELECT
+trade_agg as (
+    select
         tx_hash,
-        COUNT(*) AS num_trades,
-        SUM(surplus_usd) AS surplus_usd
-    FROM cow_protocol_{{blockchain}}.trades
-    WHERE block_date > now() - interval '{{last_n_days}}' day
-    GROUP BY tx_hash
+        count(*) as num_trades,
+        sum(surplus_usd) as surplus_usd
+    from cow_protocol_{{blockchain}}.trades
+    where block_date > now() - interval '{{last_n_days}}' day
+    group by tx_hash
 ),
 
-settled_batch_data AS (
-    SELECT
+settled_batch_data as (
+    select
         b.tx_hash,
         b.block_time,
         b.solver_address,
         b.batch_value,
         b.gas_used,
         b.dex_swaps
-    FROM cow_protocol_{{blockchain}}.batches b
-    INNER JOIN settled_batches sb ON b.tx_hash = sb.tx_hash
+    from cow_protocol_{{blockchain}}.batches as b
+    inner join settled_batches as sb on b.tx_hash = sb.tx_hash
 ),
 
-solver_info AS (
-    SELECT
-        s.name AS solver_name,
-        MAX(b.block_time) AS last_solution,
-        COUNT(*) AS batches_solved,
-        SUM(b.dex_swaps) AS dex_swaps,
-        SUM(t.num_trades) AS num_trades,
-        SUM(b.gas_used) AS gas_used,
-        SUM(b.batch_value) AS total_batch_value,
-        AVG(b.batch_value) AS average_batch_volume,
-        AVG(t.num_trades) AS average_batch_size,
-        SUM(t.surplus_usd) AS total_surplus,
-        1.0 * SUM(b.gas_used) / NULLIF(SUM(t.num_trades), 0) AS average_gas_per_trade,
-        1.0 * SUM(b.dex_swaps) / NULLIF(SUM(t.num_trades), 0) AS average_dex_swaps_per_trade
-    FROM settled_batch_data b
-    INNER JOIN cow_protocol_{{blockchain}}.solvers s
-        ON b.solver_address = s.address
-    INNER JOIN trade_agg t
-        ON b.tx_hash = t.tx_hash
-    WHERE s.environment NOT IN ('test', 'service') AND s.active = TRUE
-    GROUP BY s.name
+solver_info as (
+    select
+        s.name as solver_name,
+        max(b.block_time) as last_solution,
+        count(*) as batches_solved,
+        sum(b.dex_swaps) as dex_swaps,
+        sum(t.num_trades) as num_trades,
+        sum(b.gas_used) as gas_used,
+        sum(b.batch_value) as total_batch_value,
+        avg(b.batch_value) as average_batch_volume,
+        avg(t.num_trades) as average_batch_size,
+        sum(t.surplus_usd) as total_surplus,
+        1.0 * sum(b.gas_used) / nullif(sum(t.num_trades), 0) as average_gas_per_trade,
+        1.0 * sum(b.dex_swaps) / nulliff(sum(t.num_trades), 0) as average_dex_swaps_per_trade
+    from settled_batch_data as b
+    inner join cow_protocol_{{blockchain}}.solvers as s
+        on b.solver_address = s.address
+    inner join trade_agg as t
+        on b.tx_hash = t.tx_hash
+    where s.environment not in ('test', 'service') and s.active = TRUE
+    group by s.name
 )
 
-SELECT -- noqa: ST06
-    ROW_NUMBER() OVER (ORDER BY average_gas_per_trade) AS rk,
+select -- noqa: ST06
+    row_number() over (order by average_gas_per_trade) as rk,
     *
-FROM solver_info
-ORDER BY rk;
+from solver_info
+order by rk;
