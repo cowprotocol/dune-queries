f84d7af7ffcc0385196c6b7bb903b341913cbaea
Felix Leupold <felixleupold90@gmail.com>
2025-06-09 17:13:33 +0200

    Update monthly_dao_revenue_3700123.sql
    fixes #181

--- cowprotocol/revenue/monthly_dao_revenue_3700123.sql
+++ cowprotocol/revenue/monthly_dao_revenue_3700123.sql
@@ -14,7 +14,7 @@ cow_gnosis as (
         date_trunc('month', block_time) as date_month,
         (coalesce(sum("Limit"), 0) + coalesce(sum("Market"), 0) + coalesce(sum("UI Fee"), 0)) as protocol_fee,
         coalesce(sum("Partner Fee Share"), 0) as partner_fee_share
-    from "query_4217030(blockchain='arbitrum',ui_fee_recipient='0x6b3214fD11dc91De14718DeE98Ef59bCbFcfB432')"
+    from "query_4217030(blockchain='gnosis',ui_fee_recipient='0x6b3214fD11dc91De14718DeE98Ef59bCbFcfB432')"
     group by 1
 ),
 
