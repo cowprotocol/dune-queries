a2356c7bfec8eae62015a97c502ce4ab5b7c2927
= <bram@cow.fi>
2025-06-23 18:59:33 +0200

    Bug fixes

--- cowprotocol/accounting/token_imbalances/balance_changes_4021257.sql
+++ cowprotocol/accounting/token_imbalances/balance_changes_4021257.sql
@@ -238,14 +238,14 @@ special_balance_changes_arbitrum as ( -- noqa: ST03
 -- 2.4.1) WETH
 
 -- 2.4.1) all deposit and withdrawal events for WETH
-weth_deposits_withdrawals_base as ( -- noqa: ST03
+weth_deposits_withdrawals_base as (
     -- deposits (contract deposits ETH to get WETH)
     select
         evt_block_time as block_time,
         evt_tx_hash as tx_hash,
         contract_address as token_address,
         0x0000000000000000000000000000000000000000 as sender,
-        0x9008D19f58AAbD9eD0D60971565AA8510560ab41 as receiver,
+        0x9008d19f58aabd9ed0d60971565aa8510560ab41 as receiver,
         wad as amount
     from weth_base.WETH9_evt_Deposit
     where
@@ -266,12 +266,16 @@ weth_deposits_withdrawals_base as ( -- noqa: ST03
         and src = 0x9008d19f58aabd9ed0d60971565aa8510560ab41
 ),
 
+special_balance_changes_base as ( -- noqa: ST03
+    select * from weth_deposits_withdrawals_base
+),
+
 -- 2.4) avalanche
 -- special treatment of
 -- 2.4.1) WAVAX
 
 -- 2.4.1) all deposit and withdrawal events for WAVAX
-wavax_deposits_withdrawals_avalanche_c as ( -- noqa: ST03
+wavax_deposits_withdrawals_avalanche_c as (
     -- deposits (contract deposits AVAX to get WAVAX)
     select
         evt_block_time as block_time,
@@ -300,7 +304,7 @@ wavax_deposits_withdrawals_avalanche_c as ( -- noqa: ST03
 ),
 
 special_balance_changes_avalanche_c as ( -- noqa: ST03
-    select * from wavax_deposits_withdrawals_base
+    select * from wavax_deposits_withdrawals_avalanche_c_
 )
 
 -- combine results
