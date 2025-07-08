74e9fdfd4e6ed2e92491c4940a86a3f392d8c3eb
data-cowwboy <nitish@cow.fi>
2025-07-08 19:49:40 +0530

    lint

--- cowprotocol/accounting/rewards/auction_data_5270914.sql
+++ cowprotocol/accounting/rewards/auction_data_5270914.sql
@@ -35,17 +35,18 @@ block_data as (
         tx.last_block,
         max_block.date as max_block_date
     from txs_block_range as tx
-        inner join {{blockchain}}.blocks as max_block
-            on tx.last_block = max_block.number
-        inner join {{blockchain}}.blocks as min_block
-            on tx.last_block = min_block.number
+    inner join {{blockchain}}.blocks as max_block
+        on tx.last_block = max_block.number
+    inner join {{blockchain}}.blocks as min_block
+        on tx.last_block = min_block.number
 ),
 
 -- the following table is a restriction of the transactions table, with the goal to speed up subsequent computations
 candidate_txs as (
     select *
     from {{blockchain}}.transactions
-    where block_date >= (select min_block_date from block_data) and block_date <= (select max_block_date from block_data)
+    where
+        block_date >= (select min_block_date from block_data) and block_date <= (select max_block_date from block_data)
         and block_number >= (select first_block from txs_block_range) and block_number <= (select last_block from txs_block_range)
 ),
 
