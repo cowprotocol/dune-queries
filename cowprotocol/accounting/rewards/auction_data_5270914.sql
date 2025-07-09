48b84b7ad18623fac22fb68c5685859df13244d5
data-cowwboy <nitish@cow.fi>
2025-07-09 21:34:50 +0530

    added blocks data to use partitioning in tx (#200)
    * added blocks data to use partitioning in tx
    
    * lint
    
    * lint
    
    * lint
    
    * lint

--- cowprotocol/accounting/rewards/auction_data_5270914.sql
+++ cowprotocol/accounting/rewards/auction_data_5270914.sql
@@ -28,11 +28,26 @@ txs_block_range as (
     where block_deadline >= (select start_block from block_range) and block_deadline <= (select end_block from block_range)
 ),
 
+block_data as (
+    select
+        tx.first_block,
+        min_block.date as min_block_date,
+        tx.last_block,
+        max_block.date as max_block_date
+    from txs_block_range as tx
+    inner join {{blockchain}}.blocks as max_block
+        on tx.last_block = max_block.number
+    inner join {{blockchain}}.blocks as min_block
+        on tx.last_block = min_block.number
+),
+
 -- the following table is a restriction of the transactions table, with the goal to speed up subsequent computations
 candidate_txs as (
     select *
     from {{blockchain}}.transactions
-    where block_number >= (select first_block from txs_block_range) and block_number <= (select last_block from txs_block_range)
+    where
+        block_date >= (select min_block_date from block_data) and block_date <= (select max_block_date from block_data)
+        and block_number >= (select first_block from txs_block_range) and block_number <= (select last_block from txs_block_range)
 ),
 
 relevant_txs as (
