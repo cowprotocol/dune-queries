9945fded49a1540a1bcd351b18d7425e68bfe699
Haris Angelidakis <64154020+harisang@users.noreply.github.com>
2025-07-21 18:02:43 +0300

    Update cowprotocol/accounting/rewards/service_fee_query_4298142.sql
    Co-authored-by: Felix Henneke <felix.henneke@protonmail.com>

--- cowprotocol/accounting/rewards/service_fee_query_4298142.sql
+++ cowprotocol/accounting/rewards/service_fee_query_4298142.sql
@@ -40,7 +40,7 @@ all_cow_dao_solvers_ethereum as (
         end as solver_name,
         v.evt_block_time
     from
-        "query_5513654(blockchain='ethereum')" as v
+        "query_5143848(blockchain='ethereum')" as v
     inner join cow_protocol_ethereum.solvers as s on v.solver = s.address
     where
         v.bondingPool = 0x5d4020b9261f01b6f8a45db929704b0ad6f5e9e6
