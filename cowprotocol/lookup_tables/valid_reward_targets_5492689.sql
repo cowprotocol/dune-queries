31924995eaeb59112385e1928b38cd384c233f84
Matias <76445289+matsai5@users.noreply.github.com>
2025-07-24 11:02:18 +0200

    Create valid_reward_targets_5492689.sql

---
+++ cowprotocol/lookup_tables/valid_reward_targets_5492689.sql
@@ -0,0 +1,42 @@
+select distinct cowRewardTarget--, 'ethereum' as blockchain
+from cow_protocol_ethereum.VouchRegister_evt_Vouch
+inner join query_4056263 -- valid bonding pools
+    on pool_address = bondingPool
+    and sender = creator
+
+union distinct
+select distinct cowRewardTarget--, 'gnosis' as blockchain
+from cow_protocol_gnosis.VouchRegister_evt_Vouch
+inner join query_4056263 -- valid bonding pools
+    on pool_address = bondingPool
+    and sender = creator
+
+union distinct
+select distinct cowRewardTarget--, 'base' as blockchain
+from cow_protocol_base.VouchRegister_evt_Vouch
+inner join query_4056263 -- valid bonding pools
+    on pool_address = bondingPool
+    and sender = creator
+
+union distinct
+select distinct cowRewardTarget--, 'arbitrum' as blockchain
+from cow_protocol_arbitrum.VouchRegister_evt_Vouch
+inner join query_4056263 -- valid bonding pools
+    on pool_address = bondingPool
+    and sender = creator
+
+union distinct
+select distinct cowRewardTarget--, 'avalanche_c' as blockchain
+from cow_protocol_avalanche_c.VouchRegister_evt_Vouch
+inner join query_4056263 -- valid bonding pools
+    on pool_address = bondingPool
+    and sender = creator
+
+/*
+union distinct
+select distinct cowRewardTarget--, 'polygon' as blockchain
+from cow_protocol_polygon.VouchRegister_evt_Vouch
+inner join query_4056263 -- valid bonding pools
+    on pool_address = bondingPool
+    and sender = creator
+*/
