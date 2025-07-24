b6be31eed267cf0dade41848e86779e119c8cd1f
Matias <76445289+matsai5@users.noreply.github.com>
2025-07-24 11:52:38 +0200

    Create payout_safe_addresses_5454283.sql

---
+++ cowprotocol/lookup_tables/payout_safe_addresses_5454283.sql
@@ -0,0 +1,27 @@
+select
+    'ethereum' as blockchain,
+    0xa03be496e67ec29bc62f01a428683d7f9c204930 as address
+union all
+select
+    'gnosis' as blockchain,
+    0xa03be496e67ec29bc62f01a428683d7f9c204930 as address
+union all
+select
+    'arbitrum' as blockchain,
+    0x66331f0b9cb30d38779c786bda5a3d57d12fba50 as address
+union all
+select
+    'base' as blockchain,
+    0xa03be496e67ec29bc62f01a428683d7f9c204930 as address
+union all
+select
+    'avalanche_c' as blockchain,
+    0xa03be496e67ec29bc62f01a428683d7f9c204930 as address
+union all
+select
+    'polygon' as blockchain,
+    0x66331f0b9cb30d38779c786bda5a3d57d12fba50 as address
+union all
+select
+    'bnb' as blockchain,
+    0xa03be496e67ec29bc62f01a428683d7f9c204930 as address
