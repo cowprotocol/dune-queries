9dfe5416daf7f70d23cb64fbeb1c79230abc00e9
Matias <76445289+matsai5@users.noreply.github.com>
2025-08-05 10:59:44 +0200

    Update cow_buybacks_and_rewards_query_5273980.sql

--- cowprotocol/accounting/cow_buybacks_and_rewards/cow_buybacks_and_rewards_query_5273980.sql
+++ cowprotocol/accounting/cow_buybacks_and_rewards/cow_buybacks_and_rewards_query_5273980.sql
@@ -21,7 +21,7 @@ txs_to_exclude as (
         select
             date_trunc('{{time_frequency}}', t.evt_block_time) as time
             , sum(value / pow(10, 18)) as cow_rewarded
-        from erc20_ethereum.evt_transfer t
+        from erc20_ethereum.evt_transfer as t
         join valid_reward_targets as v
             on t."to" = v.cowrewardtarget
         join cow_token_address as cow
@@ -39,7 +39,7 @@ txs_to_exclude as (
         select
             date_trunc('{{time_frequency}}', t.evt_block_time) as time
             , sum(value / pow(10, 18)) as cow_rewarded
-        from erc20_gnosis.evt_transfer t
+        from erc20_gnosis.evt_transfer as t
         join valid_reward_targets as v
             on t."to" = v.cowrewardtarget
         join cow_token_address as cow
@@ -56,7 +56,7 @@ txs_to_exclude as (
         select
             date_trunc('{{time_frequency}}', t.evt_block_time) as time
             , sum(value / pow(10, 18)) as cow_rewarded
-        from erc20_base.evt_transfer t
+        from erc20_base.evt_transfer as t
         join valid_reward_targets as v
             on t."to" = v.cowrewardtarget
         join cow_token_address as cow
@@ -73,7 +73,7 @@ txs_to_exclude as (
         select
             date_trunc('{{time_frequency}}', t.evt_block_time) as time
             , sum(value / pow(10, 18)) as cow_rewarded
-        from erc20_arbitrum.evt_transfer t
+        from erc20_arbitrum.evt_transfer as t
         join valid_reward_targets as v
             on t."to" = v.cowrewardtarget
         join cow_token_address as cow
@@ -85,11 +85,13 @@ txs_to_exclude as (
         where
             t.evt_block_time >= date('2024-04-17')
         group by 1
-        )
+    )
     group by 1
 )
 , cow_buyback as (
-    select  time, sum(cow_bought_back) as cow_bought_back
+    select 
+        time
+        , sum(cow_bought_back) as cow_bought_back
     from (
         select
             date_trunc('{{time_frequency}}', block_date) as time
@@ -98,7 +100,7 @@ txs_to_exclude as (
             cow_protocol_ethereum.trades
         where
             trader in (0xb64963f95215fde6510657e719bd832bb8bb941b, 0x523732d31b4432bcdd4baad108f7ebe54ad478b0
-                        , 0x616de58c011f8736fa20c7ae5352f7f6fb9f0669, 0x22af3d38e50ddedeb7c47f36fab321ec3bb72a76)
+            , 0x616de58c011f8736fa20c7ae5352f7f6fb9f0669, 0x22af3d38e50ddedeb7c47f36fab321ec3bb72a76)
             and buy_token_address = 0xdef1ca1fb7fbcdc777520aa7f396b4e015f497ab
             and sell_token_address = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
             and project_contract_address = 0x9008d19f58aabd9ed0d60971565aa8510560ab41
@@ -128,8 +130,8 @@ select
     coalesce(cow_bought_back,0) as cow_bought_back,
     sum(coalesce(cow_bought_back,0)) over (order by coalesce(r.time, b.time) nulls first) as cumulative_buybacks,
     sum(coalesce(cow_rewarded,0)) over (order by coalesce(r.time, b.time) nulls first)
-        - sum(coalesce(cow_bought_back,0)) over (order by coalesce(r.time, b.time) nulls first) as net_emissions
-from solver_cow_rewards r
-full outer join cow_buyback b
+    - sum(coalesce(cow_bought_back,0)) over (order by coalesce(r.time, b.time) nulls first) as net_emissions
+from solver_cow_rewards as r
+full outer join cow_buyback as b
     on r.time = b.time
 order by 1 desc
