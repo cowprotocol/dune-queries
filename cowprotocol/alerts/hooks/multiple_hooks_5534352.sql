4da14da9bfc908e5512a850f993c8087f4d5e3a6
Matias <76445289+matsai5@users.noreply.github.com>
2025-08-06 10:57:49 +0200

    Create multiple_hooks_5534352.sql

---
+++ cowprotocol/alerts/hooks/multiple_hooks_5534352.sql
@@ -0,0 +1,29 @@
+with 
+all_hooks as (
+    select * 
+    from "query_5534333(lookback_time_unit='{{time_unit}}', lookback_units='{{units}}', blockchain='ethereum')"
+    union all
+    select * 
+    from "query_5534333(lookback_time_unit='{{time_unit}}', lookback_units='{{units}}', blockchain='gnosis')"
+    union all
+    select * 
+    from "query_5534333(lookback_time_unit='{{time_unit}}', lookback_units='{{units}}', blockchain='base')"
+    union all
+    select * 
+    from "query_5534333(lookback_time_unit='{{time_unit}}', lookback_units='{{units}}', blockchain='arbitrum')"
+    union all
+    select * 
+    from "query_5534333(lookback_time_unit='{{time_unit}}', lookback_units='{{units}}', blockchain='avalanche_c')"
+    union all
+    select * 
+    from "query_5534333(lookback_time_unit='{{time_unit}}', lookback_units='{{units}}', blockchain='polygon')"    
+)
+select *
+from (
+    select count(1) over (partition by hook_call_data) as hook_calls, *
+    from all_hooks
+)
+where 
+    hook_calls > 1 
+    and hook_app_id not in ('PERMIT_TOKEN', '1db4bacb661a90fb6b475fd5b585acba9745bc373573c65ecc3e8f5bfd5dee1f')
+order by block_time desc, order_uid
