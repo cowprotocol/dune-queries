59abbeacbbf19f9db8948f12a30e713d586d361b
Matias <76445289+matsai5@users.noreply.github.com>
2025-08-14 16:59:53 +0200

    fix json parsing in hooks_data_5534333.sql (#236)
    * fix json parsing in hooks_data_5534333.sql
    
    * Update hooks_data_5534333.sql
    
    changed field to call_data because sometimes the hook exists but has null app_id

--- cowprotocol/alerts/hooks/hooks_data_5534333.sql
+++ cowprotocol/alerts/hooks/hooks_data_5534333.sql
@@ -12,12 +12,12 @@ app_data_raw as (
 ,hooks_data as (
     select
         environment
-        ,contract_app_data as app_hash
-        ,json_extract_scalar(encode, '$.appcode') as app_code
-        ,cast(json_extract_scalar(encode, '$.metadata.bridging.destinationchainid') as int) as destination_chain_id
-        ,from_hex(substring(json_extract_scalar(encode, '$.metadata.bridging.destinationtokenaddress'), 3)) as destination_token_address
-        ,json_extract(encode, '$.metadata.hooks.post') as post_hooks_data    
-        ,json_extract(encode, '$.metadata.hooks.pre') as pre_hooks_data    
+        ,contract_app_data as app_hash        
+        ,json_extract_scalar(encode, '$.appCode') as app_code
+        ,cast(json_extract_scalar(encode, '$.metadata.bridging.destinationChainId') as int) as destination_chain_id
+        ,from_hex(substring(json_extract_scalar(encode, '$.metadata.bridging.destinationTokenAddress'), 3)) as destination_token_address
+        ,json_extract(encode, '$.metadata.hooks.post') AS post_hooks_data    
+        ,json_extract(encode, '$.metadata.hooks.pre') AS pre_hooks_data    
     from app_data_raw
 )
 ,pre_hooks as (
@@ -28,10 +28,10 @@ app_data_raw as (
         -- ,destination_chain_id
         -- ,destination_token_address
         ,'pre' as hook_type
-        ,cast(json_extract(item, '$.dappid') as varchar) as app_id 
-        ,cast(json_extract(item, '$.target') as varchar) as target
-        ,cast(json_extract(item, '$.gaslimit') as double) as gas_limit 
-        ,from_hex(substring(json_extract_scalar(item, '$.calldata'), 3)) as call_data
+        ,cast(json_extract(item, '$.dappId') as varchar) AS app_id 
+        ,cast(json_extract(item, '$.target') as varchar) AS target
+        ,cast(json_extract(item, '$.gasLimit') as double) AS gas_limit 
+        ,from_hex(substring(json_extract_scalar(item, '$.callData'), 3)) as call_data
     from hooks_data,
         unnest(cast(pre_hooks_data as array<json>)) as t(item)
 )
@@ -43,13 +43,13 @@ app_data_raw as (
         -- ,destination_chain_id
         -- ,destination_token_address
         ,'post' as hook_type
-        ,json_extract_scalar(item, '$.dappid')  as app_id 
-        ,json_extract_scalar(item, '$.target')  as target
-        ,cast(json_extract_scalar(item, '$.gaslimit') as double) as gas_limit 
-        ,from_hex(substring(json_extract_scalar(item, '$.calldata'), 3)) as call_data
+        ,json_extract_scalar(item, '$.dappId')  AS app_id 
+        ,json_extract_scalar(item, '$.target')  AS target
+        ,cast(json_extract_scalar(item, '$.gasLimit') as double) AS gas_limit 
+        ,from_hex(substring(json_extract_scalar(item, '$.callData'), 3)) as call_data
     from hooks_data,
         unnest(cast(post_hooks_data as array<json>)) as t(item)
-), 
+),
 hooks_union as (
     select * from post_hooks
     union all
@@ -83,4 +83,4 @@ left outer join traces
     on hooks.call_data = traces.input
 where 
     t.block_time >= date_add('{{lookback_time_unit}}', -{{lookback_units}}, now())
-    and hooks.app_id is not null -- makes sure the order contains a hook
+    and hooks.call_data is not null -- makes sure the order contains a hook
