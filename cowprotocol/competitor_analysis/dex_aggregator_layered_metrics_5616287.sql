036c7380264439a7b1e8eb20ba5f7d6d82e0b688
Matias <76445289+matsai5@users.noreply.github.com>
2025-08-18 14:46:57 +0200

    Update dex_aggregator_layered_metrics_5616287.sql
    fixed the traders aggregation

--- cowprotocol/competitor_analysis/dex_aggregator_layered_metrics_5616287.sql
+++ cowprotocol/competitor_analysis/dex_aggregator_layered_metrics_5616287.sql
@@ -6,7 +6,7 @@ static as (
         , '{{date_granularity}}' as date_granularity
         , (select start_date['{{period}}'] from "query_5633784") as start_date
 )
--- All fusion txs are included in '1inch' in "dex_aggregator.trades", so we'll just do a join+coalesce to differentiate
+-- All Fusion txs are in dex_aggregator.trades but tagged as 1inch 
 , fusion_txs as ( 
     select distinct
         tx_hash
@@ -18,12 +18,13 @@ static as (
         and block_time >= static.start_date
         and if(array_position(static.blockchains, '-=All=-') > 0, true, array_position(static.blockchains, blockchain) > 0) 
 )
--- Some uniswap X txs are present in "dex_aggregator.trades", we'll first filter them out to avoid double counting
+-- Some, not all uniswap x txs are in dex_aggregator.trades
 , uniswap_x_txs as ( 
     select *
     from "query_5602624(period='{{period}}')", static
     where if(array_position(static.blockchains, '-=All=-') > 0, true, array_position(static.blockchains, blockchain) > 0) 
 )
+-- Separate 1inch from fusion and remove uniswap X txs for now 
 , dex_agg_trades_prep as (
     select
         t.block_time,
@@ -37,7 +38,8 @@ static as (
         t.token_bought_symbol,
         t.token_sold_amount,
         t.token_bought_amount,
-        t.taker as trader
+        t.taker as trader,
+        static.date_granularity
     from dex_aggregator.trades as t 
     cross join static
     left join fusion_txs as f
@@ -50,64 +52,49 @@ static as (
         and if(array_position(static.blockchains, '-=All=-') > 0, true, array_position(static.blockchains, t.blockchain) > 0) 
         and u.tx_hash is null 
 )
--- Aggregate raw trades data by transaction and project
--- Also get the earliest trace position for that project within the transaction
+-- Only keep project with earliest trace_address for each tx
 , dex_agg_trades_prep_2 as (
-    select 
-        block_time
-        , blockchain
-        , tx_hash
-        , project
-        , sum(amount_usd) as volume
-        , count(distinct trader) as traders
-        , count(*) as trades
-        , min(trace_address) as proj_1st_trace_address
-        , min(evt_index) as proj_1st_evt_index
-    from dex_agg_trades_prep
-    group by 1 ,2 ,3 ,4
+    select *
+    from (
+        select 
+            *
+            , min(trace_address) over (partition by tx_hash, project) as proj_1st_trace_address
+            , min(trace_address) over (partition by tx_hash) as tx_1st_trace_address
+        from dex_agg_trades_prep
+    )
+    where proj_1st_trace_address = tx_1st_trace_address
 )
--- From all projects in a tx, keep only those with the shallowest trace_address (1st in execution)
+-- If multiple projects share the same trace_address, break ties using evt_index (note: can't do both in one go)
 , dex_agg_trades_prep_3 as (
     select *
     from (
         select 
             *
-            , min(proj_1st_trace_address) over (partition by tx_hash) as tx_1st_trace_address
+            , min(evt_index) over (partition by tx_hash, project) as proj_1st_evt_index
+            , min(evt_index) over (partition by tx_hash) as tx_1st_evt_index
         from dex_agg_trades_prep_2
     )
-    where 
-        proj_1st_trace_address = tx_1st_trace_address
-)
--- If multiple projects share the same trace_address, break ties using evt_index
-, dex_agg_trades_prep_4 as (
-    select *
-    from (
-        select 
-            *
-            , min(proj_1st_evt_index) over (partition by tx_hash) as tx_1st_evt_index
-        from dex_agg_trades_prep_3
-    )
-    where 
-        proj_1st_evt_index = tx_1st_evt_index
+    where proj_1st_evt_index = tx_1st_evt_index
 )
 , dex_agg_trades as (
     select 
-        date_trunc('{{date_granularity}}', block_time) as date
+        date_trunc(date_granularity, block_time) as date
         , project
-        , sum(volume) as volume
-        , sum(trades) as trades
-        , sum(traders) as traders
-    from dex_agg_trades_prep_4
+        , sum(amount_usd) as volume
+        , count(*) as trades
+        , count(distinct trader) as traders
+    from dex_agg_trades_prep_3
     group by 1,2
 )
-, uniswapx as ( -- add uniswap X on top since it's always 1st layer
+-- Add uniswap X on top since it's always 1st layer
+, uniswapx as ( 
     select
-        date_trunc(static.date_granularity, block_time) as date
+        date_trunc(date_granularity, block_time) as date
         , 'uniswap x' as project
         , sum(amount_usd) as volume
         , count(*) as trades
         , count(distinct trader) as traders        
-    from uniswap_x_txs, static
+    from uniswap_x_txs
     group by 1,2
 )
 select * from dex_agg_trades
