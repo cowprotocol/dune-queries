da45014dc3d5eef1f7ec8dfc7bfe2c2f8b83ddd9
Matias <76445289+matsai5@users.noreply.github.com>
2025-08-19 14:34:52 +0200

    Create parameters_mapping_5633784.sql (#239)
    * Create parameters_mapping_5633784.sql
    
    * Update parameters_mapping_5633784.sql

---
+++ cowprotocol/lookup_tables/parameters_mapping_5633784.sql
@@ -0,0 +1,15 @@
+--noqa: disable=all
+select
+    map_from_entries(
+        array[
+              ('last 7d'    , date_add('day', -7, current_date))
+            , ('last 30d'   , date_add('day', -30, current_date))
+            , ('last 3m'    , date_add('month', -3, date_trunc('month',current_date)))
+            , ('last 6m'    , date_add('month', -6, date_trunc('month',current_date)))
+            , ('last 12m'   , date_add('month', -12, date_trunc('month',current_date))) 
+            , ('last 2y'    , date_add('month', -24, date_trunc('month',current_date))) 
+            , ('last 3y'    , date_add('month', -36, date_trunc('month',current_date))) 
+            , ('last 5y'    , date_add('month', -60, date_trunc('month',current_date))) 
+            , ('all time'   , date('2010-01-01'))
+        ]
+    ) as start_date -- mapped in https://dune.com/queries/5633832 for list of parameters
