7df44880474f0bb1beb6d9b9bf180da793eafaeb
Matias <76445289+matsai5@users.noreply.github.com>
2025-09-25 15:34:08 +0200

    Update cow_token_addresses_5454278.sql

--- cowprotocol/lookup_tables/cow_token_addresses_5454278.sql
+++ cowprotocol/lookup_tables/cow_token_addresses_5454278.sql
@@ -1,8 +1,9 @@
-SELECT *
-FROM (VALUES
+select *
+from (
+    values
     ('ethereum', 0xdef1ca1fb7fbcdc777520aa7f396b4e015f497ab),
     ('gnosis',   0x177127622c4a00f3d409b75571e12cb3c8973d3c),
     ('arbitrum', 0xcb8b5cd20bdcaea9a010ac1f8d835824f5c87a04),
     ('base',     0xc694a91e6b071bf030a18bd3053a7fe09b6dae69),
     ('polygon',  0x2f4efd3aa42e15a1ec6114547151b63ee5d39958)
-) AS t(blockchain, address);
+) as t(blockchain, address)
