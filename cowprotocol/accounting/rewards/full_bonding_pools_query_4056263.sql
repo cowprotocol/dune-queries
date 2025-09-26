16138384e28d4f5eb710dd8196400d85e34d080f
Matias <76445289+matsai5@users.noreply.github.com>
2025-09-26 11:57:54 +0200

    Readability improvements on lookup tables  (#257)
    * Update cow_token_addresses_5454278.sql
    
    * Update payout_safe_addresses_5454283.sql
    
    * Update partner_fee_splits_5333695.sql
    
    * Update full_bonding_pools_query_4056263.sql
    
    * Update full_bonding_pools_query_4056263.sql
    
    * Update full_bonding_pools_query_4056263.sql
    
    * Update cow_token_addresses_5454278.sql
    
    * Update partner_fee_splits_5333695.sql
    
    * Update payout_safe_addresses_5454283.sql

--- cowprotocol/accounting/rewards/full_bonding_pools_query_4056263.sql
+++ cowprotocol/accounting/rewards/full_bonding_pools_query_4056263.sql
@@ -1,42 +1,17 @@
--- Query that hardcodes all existing full bonding pools that
---  are currently valid at CoW Protocol.
-with
-full_bonding_pools as (
-    select
-        from_hex('0x8353713b6D2F728Ed763a04B886B16aAD2b16eBD') as pool_address, -- deprecated
-        'Gnosis' as pool_name,
-        from_hex('0x6c642cafcbd9d8383250bb25f67ae409147f78b2') as creator
-    union distinct
-    select
-        from_hex('0x5d4020b9261F01B6f8a45db929704b0Ad6F5e9E6') as pool_address,
-        'CoW DAO' as pool_name,
-        from_hex('0x423cec87f19f0778f549846e0801ee267a917935') as creator
-    union distinct
-    select
-        from_hex('0xC96569Dc132ebB6694A5f0b781B33f202Da8AcE8') as pool_address, -- deprecated
-        'Project Blanc' as pool_name,
-        from_hex('0xCa99e3Fc7B51167eaC363a3Af8C9A185852D1622') as creator
-    union distinct
-    select
-        from_hex('0x7489f267C3b43dc76e4cb190F7B55ab3297706AF') as pool_address,
-        'Gnosis DAO' as pool_name,
-        from_hex('0x717e745040b9a486f2254659E8EA7Dc7d9a72A1e') as creator
-    union distinct
-    select
-        from_hex('0xe78d5F3aba2B31C980bF5E35E05B3A55b8365b48') as pool_address,
-        'Project Blanc' as pool_name,
-        from_hex('0xCa99e3Fc7B51167eaC363a3Af8C9A185852D1622') as creator
-    union distinct
-    select
-        from_hex('0x0deb0ae9c4399c51289adb1f3ed83557a56df657') as pool_address,
-        'Rizzolver' as pool_name,
-        from_hex('0x042c9c6d52881dc7e70bf3e233b540a07377d26b') as creator
-    union distinct
-    select
-        from_hex('0x7719c9c0d35d460b00487a1744394e9525e8a42c') as pool_address,
-        'Fractal' as pool_name,
-        from_hex('0xd4676b4de3a982a429a8dbe90d4a7e7cfb4769a5') as creator
-)
+-- Query that hardcodes all existing full bonding pools that are currently valid at CoW Protocol.
 
+with full_bonding_pools as (
+    select *
+    from (
+        values
+        (from_hex('0x8353713b6d2f728ed763a04b886b16aad2b16ebd'), 'Gnosis',        from_hex('0x6c642cafcbd9d8383250bb25f67ae409147f78b2')), -- deprecated
+        (from_hex('0x5d4020b9261f01b6f8a45db929704b0ad6f5e9e6'), 'CoW DAO',       from_hex('0x423cec87f19f0778f549846e0801ee267a917935')),
+        (from_hex('0xc96569dc132ebb6694a5f0b781b33f202da8ace8'), 'Project Blanc', from_hex('0xca99e3fc7b51167eac363a3af8c9a185852d1622')), -- deprecated
+        (from_hex('0x7489f267c3b43dc76e4cb190f7b55ab3297706af'), 'Gnosis DAO',    from_hex('0x717e745040b9a486f2254659e8ea7dc7d9a72a1e')),
+        (from_hex('0xe78d5f3aba2b31c980bf5e35e05b3a55b8365b48'), 'Project Blanc', from_hex('0xca99e3fc7b51167eac363a3af8c9a185852d1622')),
+        (from_hex('0x0deb0ae9c4399c51289adb1f3ed83557a56df657'), 'Rizzolver',     from_hex('0x042c9c6d52881dc7e70bf3e233b540a07377d26b')),
+        (from_hex('0x7719c9c0d35d460b00487a1744394e9525e8a42c'), 'Fractal',       from_hex('0xd4676b4de3a982a429a8dbe90d4a7e7cfb4769a5'))
+    ) as t(pool_address, pool_name, creator)
+)
 select *
 from full_bonding_pools
