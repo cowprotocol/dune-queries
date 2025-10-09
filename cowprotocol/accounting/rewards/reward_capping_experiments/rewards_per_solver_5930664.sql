cf06fe69c74260fab49cc30ad286af34184e3307
Felix Henneke <felix.henneke@protonmail.com>
2025-10-09 17:57:42 +0200

    add price improvement fee factor

--- cowprotocol/accounting/rewards/reward_capping_experiments/rewards_per_solver_5930664.sql
+++ cowprotocol/accounting/rewards/reward_capping_experiments/rewards_per_solver_5930664.sql
@@ -6,7 +6,7 @@ with rewards_per_solver as (
         sum(reward / 1e18) as reward,
         sum(new_reward_proposal / 1e18) as new_reward_proposal,
         sum(new_reward_fee_cap / 1e18) as new_reward_fee_cap
-    from "query_5787562(blockchain='{{blockchain}}',start_time='{{start_time}}',end_time='{{end_time}}',scaling='{{scaling}}',volume_fee_bps='{{volume_fee_bps}}',fixed_fee='{{fixed_fee}}')"
+    from "query_5787562(blockchain='{{blockchain}}',start_time='{{start_time}}',end_time='{{end_time}}',scaling='{{scaling}}',price_improvement_fee='{{price_improvement_fee}}',volume_fee_bps='{{volume_fee_bps}}',fixed_fee='{{fixed_fee}}')"
     group by 1
 )
 
--- cowprotocol/accounting/rewards/reward_capping_experiments/rewards_totals_5930698.sql
+++ cowprotocol/accounting/rewards/reward_capping_experiments/rewards_totals_5930698.sql
@@ -5,7 +5,7 @@ with rewards_total as (
         sum(reward / 1e18) as reward,
         sum(new_reward_proposal / 1e18) as new_reward_proposal,
         sum(new_reward_fee_cap / 1e18) as new_reward_fee_cap
-    from "query_5787562(blockchain='{{blockchain}}',start_time='{{start_time}}',end_time='{{end_time}}',scaling='{{scaling}}',volume_fee_bps='{{volume_fee_bps}}',fixed_fee='{{fixed_fee}}')"
+    from "query_5787562(blockchain='{{blockchain}}',start_time='{{start_time}}',end_time='{{end_time}}',scaling='{{scaling}}',price_improvement_fee='{{price_improvement_fee}}',volume_fee_bps='{{volume_fee_bps}}',fixed_fee='{{fixed_fee}}')"
 )
 
 select * from rewards_total
