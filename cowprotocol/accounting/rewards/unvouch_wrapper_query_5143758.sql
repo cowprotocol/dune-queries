8b886acca08d8ab045579567fde7d56bb3f55af7
harisang <harisangelidakis@gmail.com>
2025-10-28 01:54:51 +0200

    add unvouch

--- cowprotocol/accounting/rewards/unvouch_wrapper_query_5143758.sql
+++ cowprotocol/accounting/rewards/unvouch_wrapper_query_5143758.sql
@@ -17,12 +17,34 @@ with polygon_unvouching as (
     where contract_address = 0xaaa4de096d02ae21729aa31d967e148d4e3ae501 and topic0 = 0xfbe946aa1fb3fabb46cdd9f88982f3d42ef58fad11ed89cb15456b9fe4ea7d7d
 ),
 
+bnb_unvouching as (
+    select
+        'bnb' as chain, --noqa: RF04
+        contract_address,
+        tx_hash as evt_tx_hash,
+        tx_from as evt_tx_from,
+        tx_to as evt_tx_to,
+        null as evt_tx_index,
+        index as evt_index,
+        block_time as evt_block_time,
+        block_number as evt_block_number,
+        block_date as evt_block_date,
+        from_hex(substr(cast(topic2 as varchar), 27, 40)) as bondingPool,
+        from_hex(substr(cast(topic3 as varchar), 27, 40)) as sender,
+        from_hex(substr(cast(topic1 as varchar), 27, 40)) as solver
+    from bnb.logs
+    where contract_address = 0xaaa4de096d02ae21729aa31d967e148d4e3ae501 and topic0 = 0xfbe946aa1fb3fabb46cdd9f88982f3d42ef58fad11ed89cb15456b9fe4ea7d7d
+),
+
 multichain_unvouching as (
     select *
     from cow_protocol_multichain.vouchregister_evt_invalidatevouch
     union all
     select *
     from polygon_unvouching
+    union all
+    select *
+    from bnb_unvouching
 )
 
 select
