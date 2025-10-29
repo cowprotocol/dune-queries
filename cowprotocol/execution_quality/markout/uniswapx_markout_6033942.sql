c2beb3f6f0b24e019829a81a06bc6b53916d6ef7
Felix Leupold <felixleupold90@gmail.com>
2025-10-29 10:17:24 +0100

    add query

---
+++ cowprotocol/execution_quality/markout/uniswapx_markout_6033942.sql
@@ -0,0 +1,28 @@
+-- Markout calculation for UniswapX trades. Compares the price of prices.usd table at the time of the trade to the executed amounts.
+
+-- Parameters:
+--  {{blockchain}} - The chain on which trades should be counted
+--  {{start_date}} - Start date for when trades should be counted
+--  {{end_date}} - End date for when trades should be counted
+--  {{min_usd_amount}} - Minimum USD amount of the trade to be considered
+--  {{max_usd_amount}} - Maximum USD amount of the trade to be considered
+
+select 
+    tx_hash,
+    token_bought_amount,
+    token_sold_amount,
+    token_bought_address,
+    token_sold_address,
+    sell_price as sellprice,
+    buy_price as buyprice,
+    amount_usd,
+    ((token_bought_amount/token_sold_amount) / (sell_price / buy_price)) - 1.0000 as markout
+from "query_6033946(start_date='{{start_date}}', end_date='{{end_date}}')" as uniswapx
+where 
+    blockchain = '{{blockchain}}'
+    and block_time >= timestamp '{{start_date}}'
+    and block_time < timestamp '{{end_date}}'
+    and token_bought_amount * buy_price >= {{min_usd_amount}}
+    and token_bought_amount * buy_price <= {{max_usd_amount}}
+    and token_sold_amount * sell_price >= {{min_usd_amount}}
+    and token_sold_amount * sell_price <= {{max_usd_amount}}
