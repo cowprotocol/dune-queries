3ac55fa5d4c1c76063c45d5415ad255662297f9d
Felix Leupold <felixleupold90@gmail.com>
2025-10-29 10:17:00 +0100

    [Markout] Add UniswapX and 1Inch (non fusion)

--- cowprotocol/execution_quality/markout/markouts_6032683.sql
+++ cowprotocol/execution_quality/markout/markouts_6032683.sql
@@ -47,3 +47,22 @@ select
     , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
     , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
 from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='odos')"
+union all
+select 
+    'Uniswap X' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6033942(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}')"
+union distinct
+select 
+    '1inch' as project -- non intent based
+    , avg(oneinch.markout) * 10000 as avg_markout_bps
+    , approx_percentile(oneinch.markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(oneinch.markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(oneinch.markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='1inch')" as oneinch
+left join "query_6031733(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}')" as fusion
+    on oneinch.tx_hash=fusion.tx_hash
+where fusion.tx_hash is null
