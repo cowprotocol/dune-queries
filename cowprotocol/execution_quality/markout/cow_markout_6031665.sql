4b7dacfab5236eccd6f289b4e8467349574e309d
Felix Leupold <felixleupold90@gmail.com>
2025-10-29 10:08:38 +0100

    Markout Analysis (#266)
    * Markout Analysis
    
    * missing files
    
    * add percentiles

---
+++ cowprotocol/execution_quality/markout/cow_markout_6031665.sql
@@ -0,0 +1,39 @@
+-- Markout calculation for CoW Protocol trades. Compares the price of prices.usd table at the time of the trade to the executed amounts.
+
+-- Parameters:
+--  {{blockchain}} - The chain on which trades should be counted
+--  {{start_date}} - Start date for when trades should be counted
+--  {{end_date}} - End date for when trades should be counted
+--  {{min_usd_amount}} - Minimum USD amount of the trade to be considered
+--  {{max_usd_amount}} - Maximum USD amount of the trade to be considered
+
+select 
+    tx_hash,
+    token_bought_amount,
+    token_sold_amount,
+    token_bought_address,
+    token_sold_address,
+    sell.price as sellprice,
+    buy.price as buyprice,
+    amount_usd,
+    ((token_bought_amount/token_sold_amount) / (sell.price / buy.price)) - 1.0000 as markout
+from cow_protocol.trades as t
+join prices.usd as sell
+    on sell.contract_address = token_sold_address
+    and sell.minute = date_trunc('minute', block_time)
+    and sell.price > 0
+    and sell.blockchain = t.blockchain
+join prices.usd as buy
+    on buy.contract_address = token_bought_address
+    and buy.minute = date_trunc('minute', block_time)
+    and buy.price > 0
+    and buy.blockchain = t.blockchain
+where
+    t.blockchain = '{{blockchain}}'
+    and token_pair is not null
+    and block_date >= timestamp '{{start_date}}'
+    and block_date < timestamp '{{end_date}}'
+    and token_bought_amount * buy.price >= {{min_usd_amount}}
+    and token_bought_amount * buy.price <= {{max_usd_amount}}
+    and token_sold_amount * sell.price >= {{min_usd_amount}}
+    and token_sold_amount * sell.price <= {{max_usd_amount}}
---
+++ cowprotocol/execution_quality/markout/markouts_6032683.sql
@@ -0,0 +1,49 @@
+-- Markout comparison across multiple DEX aggregators and CoW Protocol
+-- Returns average markout in basis points (bps) for each project over the specified time period
+
+-- Parameters:
+--  {{blockchain}} - The chain on which trades should be counted
+--  {{start_date}} - Start date for when trades should be counted
+--  {{end_date}} - End date for when trades should be counted
+--  {{min_usd_amount}} - Minimum USD amount of the trade to be considered
+--  {{max_usd_amount}} - Maximum USD amount of the trade to be considered
+
+select
+    '1Inch Fusion' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031733(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}')"
+union distinct
+select
+    'CoW' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031665(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}')"
+union distinct
+select
+    'Kyberswap' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='kyberswap')"
+union distinct
+select
+    'Paraswap' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='paraswap')"
+union distinct
+select
+    'Odos' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='odos')"
