a3990c599c315dc47ead641628614457f53bc02e
Matias <76445289+matsai5@users.noreply.github.com>
2025-12-26 16:08:09 +0100

    Update SQL queries for final comparative markout calculations

--- cowprotocol/execution_quality/markout/markouts_6032683.sql
+++ cowprotocol/execution_quality/markout/markouts_6032683.sql
@@ -9,45 +9,21 @@
 --  {{max_usd_amount}} - Maximum USD amount of the trade to be considered
 
 select
-    '1Inch Fusion' as project
-    , avg(markout) * 10000 as avg_markout_bps
-    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
-    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
-    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
-from "query_6031733(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}')"
-union distinct
-select
     'CoW' as project
     , avg(markout) * 10000 as avg_markout_bps
     , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
     , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
     , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
 from "query_6031665(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}')"
-union distinct
+union 
 select
-    'Kyberswap' as project
+    '1inch (intent based)' as project
     , avg(markout) * 10000 as avg_markout_bps
     , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
     , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
     , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
-from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='kyberswap')"
-union distinct
-select
-    'Paraswap' as project
-    , avg(markout) * 10000 as avg_markout_bps
-    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
-    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
-    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
-from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='paraswap')"
-union distinct
-select
-    'Odos' as project
-    , avg(markout) * 10000 as avg_markout_bps
-    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
-    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
-    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
-from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='odos')"
-union all
+from "query_6031733(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}')"
+union 
 select 
     'Uniswap X' as project
     , avg(markout) * 10000 as avg_markout_bps
@@ -55,14 +31,43 @@ select
     , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
     , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
 from "query_6033942(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}')"
-union distinct
+union 
 select 
-    '1inch' as project -- non intent based
-    , avg(oneinch.markout) * 10000 as avg_markout_bps
-    , approx_percentile(oneinch.markout, 0.25) * 10000 as p25_markout_bps
-    , approx_percentile(oneinch.markout, 0.50) * 10000 as p50_markout_bps
-    , approx_percentile(oneinch.markout, 0.75) * 10000 as p75_markout_bps
-from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='1inch')" as oneinch
-left join "query_6031733(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}')" as fusion
-    on oneinch.tx_hash=fusion.tx_hash
-where fusion.tx_hash is null
+    '1inch (non intent based)' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='1inch')"
+union 
+select
+    'Kyberswap' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='kyberswap')"
+union 
+select
+    'Paraswap' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='paraswap')"
+union 
+select
+    'Odos' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='odos')"
+union 
+select
+    '0x' as project
+    , avg(markout) * 10000 as avg_markout_bps
+    , approx_percentile(markout, 0.25) * 10000 as p25_markout_bps
+    , approx_percentile(markout, 0.50) * 10000 as p50_markout_bps
+    , approx_percentile(markout, 0.75) * 10000 as p75_markout_bps
+from "query_6031688(blockchain='{{blockchain}}',start_date='{{start_date}}',end_date='{{end_date}}',min_usd_amount='{{min_usd_amount}}',max_usd_amount='{{max_usd_amount}}',project='0x')"
