833380a237907727e900ff2b7d9d0501778f5e30
harisang <harisangelidakis@gmail.com>
2026-01-19 23:47:14 +0200

    add Tsolver full bonding pool

--- cowprotocol/accounting/rewards/full_bonding_pools_query_4056263.sql
+++ cowprotocol/accounting/rewards/full_bonding_pools_query_4056263.sql
@@ -11,6 +11,7 @@ with full_bonding_pools as (
         (from_hex('0xe78d5f3aba2b31c980bf5e35e05b3a55b8365b48'), 'Project Blanc', from_hex('0xca99e3fc7b51167eac363a3af8c9a185852d1622')),
         (from_hex('0x0deb0ae9c4399c51289adb1f3ed83557a56df657'), 'Rizzolver',     from_hex('0x042c9c6d52881dc7e70bf3e233b540a07377d26b')),
         (from_hex('0x7719c9c0d35d460b00487a1744394e9525e8a42c'), 'Fractal',       from_hex('0xd4676b4de3a982a429a8dbe90d4a7e7cfb4769a5'))
+        (from_hex('0x8c77268262cd0de611e543dfd7e9b496793acb86'), 'Tsolver',       from_hex('0x57db889be7fac15601d04714f34e1525186545e2'))
     ) as t(pool_address, pool_name, creator)
 )
 select *
