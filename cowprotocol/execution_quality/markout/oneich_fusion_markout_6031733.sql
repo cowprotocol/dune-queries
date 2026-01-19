a32852801a9cc394012c8c08acc8d79b64a13644
Matias <76445289+matsai5@users.noreply.github.com>
2026-01-19 10:13:01 +0100

    Revert "Add markout queries over time (#306)" (#319)
    This reverts commit f99a77b616319cfe0a10ff97a9c80b2899981166.

--- cowprotocol/execution_quality/markout/oneich_fusion_markout_6031733.sql
+++ cowprotocol/execution_quality/markout/oneich_fusion_markout_6031733.sql
@@ -1,4 +1,4 @@
--- 1inch intent based modes markout calculation. Compares the price of prices.usd table at the time of the trade to the executed amounts
+-- 1inch fusion markout calculation. Compares the price of prices.usd table at the time of the trade to the executed amounts
 
 -- Parameters:
 --  {{blockchain}} - The chain on which trades should be counted
@@ -8,30 +8,30 @@
 --  {{max_usd_amount}} - Maximum USD amount of the trade to be considered
 
 select
-    block_time,
     tx_hash,
-    cast(dst_executed_amount as double) /pow(10, cast(complement['dst_decimals'] as bigint)) as dst_exec_amount,
-    cast(src_executed_amount as double) /pow(10, cast(complement['src_decimals'] as bigint)) as src_exec_amount,
+    dst_token_amount,
+    src_token_amount,
     dst_token_address,
     src_token_address,
     sell.price as sellprice,
     buy.price as buyprice,
     amount_usd,
-    (cast(dst_executed_amount as double) /pow(10, cast(complement['dst_decimals'] as bigint))) / (cast(src_executed_amount as double)/pow(10, cast(complement['src_decimals'] as bigint))) / (sell.price / buy.price) - 1.0000 as markout
+    ((dst_token_amount/pow(10,dst_token_decimals))/(src_token_amount/pow(10,src_token_decimals)) / (sell.price / buy.price)) - 1.0000 as markout
 from oneinch.swaps as t
 join prices.usd as sell
     on sell.contract_address = src_token_address
     and sell.minute = date_trunc('minute', block_time)
     and sell.price > 0
-    and sell.blockchain = '{{blockchain}}'
+    and sell.blockchain = t.blockchain
 join prices.usd as buy
     on buy.contract_address = dst_token_address
     and buy.minute = date_trunc('minute', block_time)
     and buy.price > 0
-    and buy.blockchain = '{{blockchain}}'
+    and buy.blockchain = t.blockchain
 where
     t.blockchain = '{{blockchain}}'
     and block_time >= timestamp '{{start_date}}'
     and block_time < timestamp '{{end_date}}'
-    and amount_usd between {{min_usd_amount}} and {{max_usd_amount}}
-    and mode in ('fusion', 'cross_chain')
+    and amount_usd <= {{max_usd_amount}}
+    and amount_usd >= {{min_usd_amount}}
+    and flags['fusion']
