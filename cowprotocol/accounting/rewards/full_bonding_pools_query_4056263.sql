16d936f6f05e66fb90b3bcdf0465d546ad2021fd
Haris Angelidakis <64154020+harisang@users.noreply.github.com>
2026-01-20 10:22:50 +0200

    Add Tsolver full bonding pool (#322)
    * add Tsolver full bonding pool
    
    * fix typo

--- cowprotocol/accounting/rewards/full_bonding_pools_query_4056263.sql
+++ cowprotocol/accounting/rewards/full_bonding_pools_query_4056263.sql
@@ -10,7 +10,8 @@ with full_bonding_pools as (
         (from_hex('0x7489f267c3b43dc76e4cb190f7b55ab3297706af'), 'Gnosis DAO',    from_hex('0x717e745040b9a486f2254659e8ea7dc7d9a72a1e')),
         (from_hex('0xe78d5f3aba2b31c980bf5e35e05b3a55b8365b48'), 'Project Blanc', from_hex('0xca99e3fc7b51167eac363a3af8c9a185852d1622')),
         (from_hex('0x0deb0ae9c4399c51289adb1f3ed83557a56df657'), 'Rizzolver',     from_hex('0x042c9c6d52881dc7e70bf3e233b540a07377d26b')),
-        (from_hex('0x7719c9c0d35d460b00487a1744394e9525e8a42c'), 'Fractal',       from_hex('0xd4676b4de3a982a429a8dbe90d4a7e7cfb4769a5'))
+        (from_hex('0x7719c9c0d35d460b00487a1744394e9525e8a42c'), 'Fractal',       from_hex('0xd4676b4de3a982a429a8dbe90d4a7e7cfb4769a5')),
+        (from_hex('0x8c77268262cd0de611e543dfd7e9b496793acb86'), 'Tsolver',       from_hex('0x57db889be7fac15601d04714f34e1525186545e2'))
     ) as t(pool_address, pool_name, creator)
 )
 select *
