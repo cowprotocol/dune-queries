6d511136950fb5d7e7d9c9a9c7c1319d72b3f513
Matias <76445289+matsai5@users.noreply.github.com>
2026-04-14 17:38:24 +0200

    minor formatting change for consistency

--- cowprotocol/alerts/hooks/skipped_hooks_5534339.sql
+++ cowprotocol/alerts/hooks/skipped_hooks_5534339.sql
@@ -40,7 +40,6 @@ where
         '1db4bacb661a90fb6b475fd5b585acba9745bc373573c65ecc3e8f5bfd5dee1f',
         'cow.fi',
         'Fordefi',
-        'UNI_V2_DEPOSIT'
-        )
+        'UNI_V2_DEPOSIT')
     )
     and not(hook_app_id like 'cow-sdk://flashloans/aave%')
