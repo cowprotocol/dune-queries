2a818ef2c071c6b86200d1c95bb2c61467e26d15
Bram van den Berg <bram@cow.fi>
2026-04-16 21:49:23 +0900

    Fix bug (#352)

--- cowprotocol/accounting/rewards/excluded_batches_query_3490353.sql
+++ cowprotocol/accounting/rewards/excluded_batches_query_3490353.sql
@@ -339,7 +339,7 @@ where
     tx_hash = 0x193E147809099DE19B08EE6B7AAD699507C176AFACE8B47233473909BA948D1F
 
     -- for week of April 07 - April 14, 2026 on BNB
-    tx_hash = 0xe3ccdb8c162b929fda198c04c2944f185bc02d5ffbc543044111cda3594b9a63
+    or tx_hash = 0xe3ccdb8c162b929fda198c04c2944f185bc02d5ffbc543044111cda3594b9a63
 
 -- GNOSIS
 union all
