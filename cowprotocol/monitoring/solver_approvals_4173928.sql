67c2b213d8f197ed2f98d0fb813ebdb128703161
tamir-cow <tamir@cow.fi>
2026-07-07 08:55:09 +0300

    Add SQL query for solver approvals monitoring (#360)
    * Add SQL query for solver approvals monitoring
    
    * Refactor SQL query for solver approvals
    
    * Update responsible_address to use solver_address
    
    * Refactor SQL query for solver approvals
    
    * Delete cowprotocol/monitoring/solver_approvals.sql

--- cowprotocol/monitoring/solver_approvals_4173928.sql
+++ cowprotocol/monitoring/solver_approvals_4173928.sql
@@ -2,7 +2,6 @@ with ranked as (
     select
         evt_tx_hash as tx_hash,
         evt_block_time as block_time,
-        "from" as responsible_address,
         spender,
         contract_address as token,
         a.value,
@@ -11,7 +10,6 @@ with ranked as (
     inner join {{blockchain}}.transactions on evt_tx_hash = hash
     where owner = 0x9008d19f58aabd9ed0d60971565aa8510560ab41
 ),
-
 solvers as (
     select
         address,
@@ -21,19 +19,19 @@ solvers as (
     from dune.cowprotocol.solvers
     where blockchain = '{{blockchain}}'
 )
-
 select --noqa: ST06
-    block_time,
-    tx_hash,
-    responsible_address,
+    r.block_time,
+    r.tx_hash,
+    b.solver_address as responsible_address,
     case
-        when responsible_address = 0x05c5494572e4ab2d48d3ab3aaf6bd4e7b1c98382 or responsible_address = 0xd8ca5fe380b68171155c7069b8df166db28befdd then 'PROPOSER-ACCOUNT'
-        else coalesce(concat(environment, '-', name), 'NON-SOLVER')
+        when b.solver_address = 0x05c5494572e4ab2d48d3ab3aaf6bd4e7b1c98382 or b.solver_address = 0xd8ca5fe380b68171155c7069b8df166db28befdd then 'PROPOSER-ACCOUNT'
+        else coalesce(concat(s.environment, '-', s.name), 'NON-SOLVER')
     end as responsible_solver,
-    spender,
-    token,
-    value
+    r.spender,
+    r.token,
+    r.value
 from ranked as r
-left outer join solvers as s on r.responsible_address = s.address
-where rk = 1 and value > 0
-order by block_time desc
+inner join cow_protocol_{{blockchain}}.batches as b on r.tx_hash = b.tx_hash
+left outer join solvers as s on b.solver_address = s.address
+where r.rk = 1 and r.value > 0
+order by r.block_time desc
