dad4d63374b44c795e3435310c7f755cb8e16d5b
Bram <bram@cow.fi>
2026-08-06 09:05:52 +0200

    Update fees query as well

--- cowprotocol/accounting/fees/classified_fees_4058574.sql
+++ cowprotocol/accounting/fees/classified_fees_4058574.sql
@@ -43,6 +43,7 @@ protocol_fees as (
         case
             when '{{blockchain}}' = 'ethereum' and protocol_fee_token_address = 0x3231cb76718cdef2155fc47b5286d82e6eda273f then 0x39b8b6385416f4ca36a20319f70d28621895279d
             when '{{blockchain}}' = 'gnosis' and protocol_fee_token_address = 0xcb444e90d8198415266c6a2724b7900fb12fc56e then 0x420ca0f9b9b604ce0fd9c18ef134c705e5fa3430
+            when '{{blockchain}}' = 'gnosis' and sell_token_address = 0x5cb9073902f2035222b9749f8fb0c9bfe5527108 then 0x8e34bfec4f6eb781f9743d9b4af99cd23f9b7053
             else protocol_fee_token_address
         end as token_address,
         protocol_fee as amount,
