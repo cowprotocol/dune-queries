031a8cfc844287525d3a14cc0f2579c80ade0a59
Haris Angelidakis <64154020+harisang@users.noreply.github.com>
2026-08-25 10:10:21 +0200

    ignore bnb tx hash on token imbalances query that causes overflow (#381)
    * ignore bnb tx hash on token imbalances query
    
    * fix in right place
    
    * fix linting

--- cowprotocol/accounting/token_imbalances/imbalances_4021644.sql
+++ cowprotocol/accounting/token_imbalances/imbalances_4021644.sql
@@ -11,7 +11,7 @@ select
     block_time,
     tx_hash,
     token_address,
-    sum(
+    sum( -- noqa: PRS
         case
             when sender = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41 and receiver = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41 then cast(0 as int256)
             when receiver = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41 then cast(amount as int256)
@@ -19,4 +19,5 @@ select
         end
     ) as amount
 from "query_4021257(blockchain='{{blockchain}}',start_time='{{start_time}}',end_time='{{end_time}}')"
+where tx_hash != 0xf06378e10b27ccc35de3c8c2ba53fc6da2a5c837d9be5b10a584ab24dfa8a710
 group by block_time, tx_hash, token_address
